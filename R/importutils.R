249be74d80ef95265cf2abe81391aede8324fda9
fgochez <fgochez@mango-solutions.com>
2009-09-09 10:03:12 +0000

    replaced a call to "stop" with one to "RNMImportStop", and ".cat" to "logMessage"
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@10699 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/importutils.R
+++ R/importutils.R
@@ -116,8 +116,8 @@ dirname.abs <- function(x){
 	}
 	if(test) test <- file.exists( file )
 	if( any(test) ){
-		if(stop) stop(paste("missing files", file[test], sep = "\n\t\t"))
-		if(warn) .cat("Warning: Cannot open file", file[test])
+		if(stop) RNMImportStop(paste("missing files", file[test], sep = "\n\t\t"))
+		if(warn) logMessage("Warning: Cannot open file", file[test], "warnings")
 		if(remove) file <- file[!test]
 	}
 	if( !length(file) ) file <- NULL
