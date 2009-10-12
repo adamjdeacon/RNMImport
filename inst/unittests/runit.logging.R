795e2754a1323a1908265491f9353844e0870ad7
fgochez <fgochez@mango-solutions.com>
2009-10-12 10:48:24 +0000

    Modified unit tests to test that closing unopened logs generates the correct error.  This tests issue 0001157
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@12060 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.logging.R
+++ inst/unittests/runit.logging.R
@@ -2,7 +2,22 @@
 test.logging <- function()
 {
 	checkEquals(availableLogs(), c("stdReport", "detailedReport", "warnings", "highLevelParse", "lowLevelParse"))
+	# should throw an exception
 	checkException(setLogConnection("fakeLog", NULL), msg = "Testing error-handling")
+	
+	# try closing logs that are not open for the correct errors
+	# this checks against ticket 0001157 
+	
+	z <- try(closeLogConnection("warnings", FALSE))
+	
+	 checkEquals(as.character(z),  "Error in closeLogConnection(logName = \"warnings\", allowFail = FALSE) : \n  Unable to close the connection warnings \n\n"
+					, msg = " correct error message when trying to close log without sucess" )
+	
+	closeLogConnection("warnings", TRUE)
+	# failure is now allowed without generating an exception, so check if the warning is true
+	# TODO: the following looks like it should work, but does not for some reason.  Fix it.
+	# checkEquals(names(last.warning)[1],  "Unable to close the connection warnings , but proceeding anyway \n" )
+			
 	setLogFile("stdReport", file.path(unitTestPath, "testdata/testlog.txt"))
 	logMessage("test", "stdReport")
 	checkEquals("test", scan(file.path(unitTestPath, "testdata/testlog.txt"), what = ""))
