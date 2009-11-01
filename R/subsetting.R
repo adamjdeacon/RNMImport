c19488f877f401c1af6b6b8a7034fc3d6c8e2051
fgochez <fgochez@mango-solutions.com>
2009-11-01 17:44:22 +0000

    Added "augmentDataSubset" and comments.  This addresses mantis issue 1594.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@12837 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/subsetting.R
+++ R/subsetting.R
@@ -1,18 +1,46 @@
-# TODO: Add comment
-# 
-# Author: fgochez
+# $Rev$
+# $LastChangedDate$
+#
 ###############################################################################
 
-# TODO: unit tests
 
 # TODO: make this a generic function later
 
+#' Sets the subset of an object
+#' @param x Object whose subset should be modified.
+#' @param value The subset to attach
+#' @return None
+#' @author fgochez
+#' @export
+
 "dataSubset<-" <- function(x, value)
 {
+	RNMImportStopifnot(inherits(x, "data.frame") |  inherits(x, "NMProblem"), "x is not of the correct class")
+	if(is.null(value))
+		return(x)
+	assertClass(value, "character")
 	attr(x, "dataSubset") <- value
 	x
 }
-# TODO: mantual entry
+
+#' Adds subset expressions to the subset of an object
+#' @param x Object whose subset should be modified.
+#' @param value The subset to attach
+#' @return None
+#' @author fgochez
+#' @export
+
+"augmentDataSubset<-" <- function(x, value)
+{
+	RNMImportStopifnot(inherits(x, "data.frame") |  inherits(x, "NMProblem"), "x is not of the correct class")
+	if(is.null(value))
+		return(x)
+	assertClass(value, "character")
+	newSubset <- unique(c(dataSubset(x), value ))
+	dataSubset(x) <- newSubset
+	x
+}
+
 # TODO: take this out of RNMGraphics later
 
 "graphSubset<-" <- function(x, value)
@@ -21,20 +49,38 @@
 	x
 }
 
+#' Retrieves the data subset associated with a particular object
+#' @param x Object (data.frame or NMProblem)
+#' @return Character vector of expressions
+#' @author fgochez
+#' @export
+
 dataSubset <- function(x)
 {
 	attr(x, "dataSubset")
 }
 
+#' Applies a set of subset expressions to a data.frame
+#' @param obj A data.frame to be subsetted
+#' @param sub Character vector of subsets to apply
+#' @return subsetted data.frame, with subsets in sub applied
+#' @author fgochez
+#' @export
+
 applyDataSubset <- function(obj, sub = NULL)
 {
+	assertClass(obj, "data.frame")
 	if(is.null(sub))
 		return(obj)
 	
+	assertClass(sub, "character")
 	for(x in sub)
 	{
 		res <- try(subset(obj, eval(parse(text = x)) ), silent = TRUE)
 		if(!inherits(res, "try-error")) obj <- res
+		else{
+			RNMImportWarning(paste("Unable to apply subset:",x,"\n" ), call = match.call())
+		}
 		
 	}
 	return(obj)	
