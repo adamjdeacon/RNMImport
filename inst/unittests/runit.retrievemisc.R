8b375d448ec9c94f13566533a1bad9f1fbd41118
fgochez <fgochez@mango-solutions.com>
2009-11-18 16:31:42 +0000

    Unit tests for getNmVersion (on NMRun, NMBasicModel and NMSimModel).  Addresses issue 0001730.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13262 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.retrievemisc.R
+++ inst/unittests/runit.retrievemisc.R
@@ -1,4 +1,7 @@
 
+# tests retrieval of objective function
+# getObjective
+
 test.getObjective <- function()
 {
 	setNmPath("internalunit",  file.path(unitTestPath, "testdata/TestRun") )
@@ -17,5 +20,25 @@ test.getObjective <- function()
 	checkEquals(obj, target)
 	
 	removeNmPath("internalunit")
+	
+}
+
+# tests the following functions:
+# getNmVersion
+#
+
+test.getMisc <- function()
+{
+	run1 <- importNm(conFile = "testdata1notab.ctl", reportFile = "testdata1notab.lst", 
+			path = file.path(unitTestPath, "testdata/TestRunNoTab"))
+	run2 <- importNm(conFile = "TestData1SIM.con", reportFile = "TestData1SIM.lst", 
+			path = file.path(unitTestPath, "testdata/TestSimRun"))
+	
+	checkEquals( getNmVersion(run1), c(major = "VI", "minor" = "1" ) , " version of run1 is correct")
+	checkEquals( getNmVersion(run2), c(major = "VI", "minor" = "1" ) , " version of run2 is correct")
+	prob1 <- getProblem(run1)
+	prob2 <- getProblem(run2)
+	checkEquals( getNmVersion(prob1), c(major = "VI", "minor" = "1" ) , " version of run1 problem is correct")
+	checkEquals( getNmVersion(prob2), c(major = "VI", "minor" = "1" ) , " version of run2 problem is correct")
 	
 }
