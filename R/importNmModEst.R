3842abe610fd72a461bcd725fd351e1175be35da
fgochez <fgochez@mango-solutions.com>
2009-11-24 10:32:41 +0000

    Parses the $EST statements of a NONMEM 7 control file.  Partially addresses issue 1771
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13393 4c17f647-02f2-7e4c-97ba-2b8bca437fff

---
+++ R/importNmModEst.R
@@ -0,0 +1,19 @@
+#
+# $LastChangedDate$
+# $LastChangedBy$
+# $Rev$
+# Author: fgochez
+###############################################################################
+
+
+#' Parse $EST statements from a control file and return a list of parsed statements (one element for each $EST) 
+#' At the moment, these are just strings, but this will likely change
+#' @param txt A vector of strings containing the text of the $EST statements to be parsed 
+#' @return A list of parsed statements
+#' @author fgochez
+
+
+.importNmModEst <- function(txt )
+{
+	section(txt, "EST", oneline = TRUE, as.list = TRUE)
+}
