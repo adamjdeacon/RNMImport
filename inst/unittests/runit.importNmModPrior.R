0ad8d4eb2b5d97a10e49efb449a8dc576a802a2a
fgochez <fgochez@mango-solutions.com>
2009-11-24 09:11:04 +0000

    .importNmMod prior was not an exposed function, so the unit test was failing.  Related to issue 0001729.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13387 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.importNmModPrior.R
+++ inst/unittests/runit.importNmModPrior.R
@@ -1,7 +1,7 @@
 #
-# $Rev: $
-# $LastChangedDate: $
-# $LastChangedBy: $
+# $Rev$
+# $LastChangedDate$
+# $LastChangedBy$
 ###############################################################################
 
 # tests the .importNmModPrior function
@@ -12,14 +12,14 @@ test.importNmModPrior <- function()
 	# simple test to see if 2 $PRIOR statements are correctly parsed
 	
 	priorText <- "$PRIOR NWPRI NTHETA=5, NETA=4, NTHP=0, NETP=4, NPEXP=1"
-	priorTest1 <- .importNmModPrior(priorText)
+	priorTest1 <- RNMImport:::.importNmModPrior(priorText)
 	
 	expected1 <- c(nTheta = 5, nEta = 4, nThp = 0,  npExp = 1, nEtp = 4)
 	attr(expected1, "NWPRI") <- TRUE
 	checkEquals(priorTest1, expected1 , "| Prior statement correctly parsed" )
 	
 	priorText2 <- "$PRIOR NWPRI NTHETA=2, NETA=5, NTHP=0, NETP=5, NPEXP=1"
-	priorTest2 <- .importNmModPrior(priorText2)
+	priorTest2 <- RNMImport:::.importNmModPrior(priorText2)
 	
 	expected2 <- c(nTheta = 2, nEta = 5, nThp = 0,  npExp = 1, nEtp = 5)
 	attr(expected2, "NWPRI") <- TRUE
