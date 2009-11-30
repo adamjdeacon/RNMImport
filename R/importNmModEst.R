da2fa19a6cf2ffb375720008238b7c164bdc2756
fgochez <fgochez@mango-solutions.com>
2009-11-30 15:52:51 +0000

    Expanded so that it now extracts the FILE and METHOD fields from each EST statement (spec updated), and returns a matrix.  Related to issue 1771
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13657 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/importNmModEst.R
+++ R/importNmModEst.R
@@ -9,11 +9,29 @@
 #' Parse $EST statements from a control file and return a list of parsed statements (one element for each $EST) 
 #' At the moment, these are just strings, but this will likely change
 #' @param txt A vector of strings containing the text of the $EST statements to be parsed 
-#' @return A list of parsed statements
+#' @return A matrix of parsed statements.  Each row of the matrix will correspond to one parsed $EST statement
+#' 
 #' @author fgochez
 
 
 .importNmModEst <- function(txt )
 {
-	section(txt, "EST", oneline = FALSE, stripout = TRUE, glue = TRUE)
+	estSectionText <- section(txt, "EST", oneline = FALSE, stripout = TRUE, glue = TRUE)
+	
+	.parseEst <- function(estElement)
+	{
+		
+		x <- equalExpressionPop(estElement, "METHOD", shortcut = TRUE, inPlace = FALSE)
+		meth <- x$op.out
+		
+		x <- equalExpressionPop(x$txt, "FILE", shortcut = TRUE, inPlace = FALSE)
+		fileName <- if(!is.null(x$op.out)) x$op.out else ""
+		remainingText <- x$txt
+		
+		c("method" = meth, "file" = fileName, "remainingText" = remainingText)
+
+	}
+	
+	parsedEstSections <- sapply(estSectionText, .parseEst)
+	t(parsedEstSections)
 }
