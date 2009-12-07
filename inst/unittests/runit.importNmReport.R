c1cf0ff51188c2817b969274e4360d9daeda9dc8
fgochez <fgochez@mango-solutions.com>
2009-12-07 15:13:46 +0000

    Updated the test to take into account the change made in issue 1867.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13853 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.importNmReport.R
+++ inst/unittests/runit.importNmReport.R
@@ -1,3 +1,4 @@
+# TODO: make these tests more comprehensive!!
 # Note: a great deal of the functionality of importNmReport is covered by other unit tests, such
 # as importNm, so these tests will not be extensive
 
@@ -5,12 +6,13 @@
 test.importNmReport.Basic <- function()
 {
 	report1 <- importNmReport("TestData1.lst", path = file.path(unitTestPath, "testdata/TestRun"))
+	checkTrue("VersionInfo" %in% names(report1), msg = " |VersionInfo is a property of the entire report")
 	conStatements <- importNmMod("TestData1.ctl", path = file.path(unitTestPath, "testdata/TestRun"))
 	report1.withCtl <- importNmReport("TestData1.lst", controlStatements = conStatements, path = file.path(unitTestPath, "testdata/TestRun"))
 	checkEquals(report1, report1.withCtl, "report loaded with control statements and without are identical (1)")
 	checkEquals(length(report1$Raw), 166, msg = "Raw contents correct length")
 	probRes <- report1$problemResults
-	checkEquals(names(probRes[[1]]), c("VersionInfo", "nRecords", "nIndividuals", "Objective.Minimum", "FinalEstimates","Iter" ), 
+	checkEquals(names(probRes[[1]]), c("nRecords", "nIndividuals", "Objective.Minimum", "FinalEstimates","Iter" ), 
 			"checking presence of correct elements in report")
 	
 	
@@ -25,6 +27,6 @@ test.importNmReport.SimModel <- function()
 	
 	checkEquals(report2, report2.withCtl, "report loaded with control statements and without are identical (2)")
 	probRes <- report2$problemResults
-	checkEquals(names(probRes[[1]]), c("VersionInfo", "nRecords", "nIndividuals", "FinalEstimates"), "checking presence of correct elements in report" )
+	checkEquals(names(probRes[[1]]), c("nRecords", "nIndividuals", "FinalEstimates"), "checking presence of correct elements in report" )
 	
 }
