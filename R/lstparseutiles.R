1fd3cb426c4f56b5d123669d063117fdc68a692b
fgochez <fgochez@mango-solutions.com>
2009-12-10 13:15:19 +0000

    Added various comments for greater clarity.  Also implemented logic for partitioning a NONMEM 7 report file's "#METH" blocks.  Related to issue 1860.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13932 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/lstparseutiles.R
+++ R/lstparseutiles.R
@@ -1,3 +1,54 @@
+# $LastChangedDate$
+# $LastChangedBy$
+# $Rev$
+
+#' Splits a single block of #METH statements into "sections".  This is similar to sectionLst, but only for a
+#' single block delimeted by #METH in NONMEM 7
+#' @title Split method text block into sections
+#' @param [C,+] methodContents Line of text in a #METH statement (includes the #METH line)
+#' @return [L, +] a named list of split text, with names corresponding to section names
+#' @author fgochez
+
+sectionMethodBlock <- function(methodContents)
+{
+	#
+	SECTIONSEP <- "1"
+	
+	# strip blank lines
+	
+	methodContentsClean <- negGrep("^[[:space:]]*$", methodContents, value = TRUE) 
+	sectionStarts <- grep(methodContentsClean, pattern = paste("^[[:space:]]*", SECTIONSEP, "[[:space:]]*$", sep = "" ))
+	RNMImportStopifnot(length(sectionStarts) >= 1, match.call())
+	# grab the section "header" text block
+	
+	headerBlock <- methodContentsClean[1:(sectionStarts[1] - 1)]
+	
+	methodContentsNoHeader <- tail(methodContentsClean, sectionStarts[1] - 1)
+	
+	# split the rest into chunks of text that begin with "1"
+	
+	blockSplits <- splitVector(methodContentsClean, sectionStarts, includeEnd = FALSE)
+	# now decompose those text blocks into sections using the nmTitle function
+	blockSplits <- lapply(blockSplits, 
+			function(x) {
+				# the first line of each "split" block is a "1" delimeter, which should be removed, hence the x[-1]
+				# NONMEM 7 has titles of 6 lines, rather than 5 for NONMEM VI 
+				xDecomposed <- nmTitle(x[-1], titleSecLength = 6)
+				y <- xDecomposed$title
+				RNMImportStopifnot(length(y) <= 2)
+				# second element of "title" is the actual title
+				y <- if(length(y) == 2) y[2] else y
+				z <- xDecomposed$text
+				list(title = y, text = z)
+			})
+	
+	blockContents <- lapply(blockSplits, function(x) x$text)
+	names(blockContents) <- sapply(blockSplits, function(x) x$title )
+	
+	blockContents
+	
+}
+
 # TODO: this function is overly-complex; clean it so that it no longer recurses
 
 #' Partitions the contents of a list file into sections that can then be parsed individually
@@ -100,15 +151,65 @@ sectionLst <- function( fileContents, sep,
 	} else if( length(li) > 1 ) li else li[[1]] 
 }
 
-nmTitle <- function( txt ){
+#' Splits a chunk of report file section into its title/header and its title.
+#' @title split list text into sections
+#' @param txt [C,+] The input sectioned chunk of the lst file.  This is typically obtained from sectionLst or
+#' sectionMethodBlocks.  See the comment block below for examples of section text 
+#' @param titleSecLength [N,1] 
+#' @param maxTitleLines [N,1]   
+#' @return a list with two elements: "text" and "title".  The latter is the title of the section 
+#' (e.g. "FINAL PARAMETER ESTIMATE"), the former is the text content without the titles
+#' @author fgochez
+
+# Example section text:
+#
+#[1] " ************************************************************************************************************************"
+#[2] " ********************                                                                                ********************"
+#[3] " ********************                                  FINAL PARAMETER ESTIMATE                      ********************"
+#[4] " ********************                                                                                ********************"
+#[5] " ************************************************************************************************************************"
+#[6] " THETA - VECTOR OF FIXED EFFECTS PARAMETERS   *********"                                                                  
+#[7] "            TH 1      TH 2      TH 3      TH 4"                                                                           
+#[8] "         2.78E+00  3.02E-01  4.90E-01  1.14E+01"                                                                          
+#[9] " OMEGA - COV MATRIX FOR RANDOM EFFECTS - ETAS  ********"                                                                  
+#[10] "            ETA1      ETA2      ETA3      ETA4"                                                                           
+#[11] " ETA1"                                                                                                                    
+#[12] "+        1.69E-01"                                                                                                        
+#[13] " ETA2"                                                                                                                    
+#[14] "+        0.00E+00  1.08E-02"                                                                                              
+#[15] " ETA3"                                                                                                                    
+#[16] "+        0.00E+00  0.00E+00  9.42E-02"                                                                                    
+#[17] " ETA4"                                                                                                                    
+#[18] "+        0.00E+00  0.00E+00  0.00E+00  9.87E-02"                                                                          
+#[19] " SIGMA - COV MATRIX FOR RANDOM EFFECTS - EPSILONS  ****"                                                                  
+#[20] "            EPS1"                                                                                                         
+#[21] " EPS1"                                                                                                                    
+#[22] "+        1.97E-02"  
+
+# expected output is:
+
+# $text:
+#[1] " THETA - VECTOR OF FIXED EFFECTS PARAMETERS   *********" "            TH 1      TH 2      TH 3      TH 4"         
+#[3] "         2.78E+00  3.02E-01  4.90E-01  1.14E+01"         " OMEGA - COV MATRIX FOR RANDOM EFFECTS - ETAS  ********"
+#[5] "            ETA1      ETA2      ETA3      ETA4"          " ETA1"                                                  
+#[7] "+        1.69E-01"                                       " ETA2"                                                  
+#[9] "+        0.00E+00  1.08E-02"                             " ETA3"                                                  
+#[11] "+        0.00E+00  0.00E+00  9.42E-02"                   " ETA4"                                                  
+#[13] "+        0.00E+00  0.00E+00  0.00E+00  9.87E-02"         " SIGMA - COV MATRIX FOR RANDOM EFFECTS - EPSILONS  ****"
+#[15] "            EPS1"                                        " EPS1"                                                  
+#[17] "+        1.97E-02"                    
+
+# $title: `FINAL PARAMETER ESTIMATE`
+
+nmTitle <- function( txt, titleSecLength = 5, maxTitleLines = 2 ){
 	if( is.list(txt)  ) return( list( title = NULL ) )
 	
 	if(any(regexMatches( txt, rx = "\\*{15,}" )))
 	{
-		titleText <- grep("\\*{15,}", txt[1:5], value = TRUE )
+		titleText <- grep("\\*{15,}", txt[1:titleSecLength], value = TRUE )
 		titleText <- gsub( "\\*+", "", titleText )
 		titleText <- stripBlanks( titleText, remove = TRUE)
-		txt <- txt[-(1:5)]
+		txt <- txt[-(1:titleSecLength)]
 	} 
 	else 
 	{
@@ -142,7 +243,7 @@ nmVersion <- function(lstContents)
 
 #' 
 #' @param contents A character vector of strings holding the lines of subproblem information (ONLY!) for one simulation problem
-#' @title
+#' @title 
 #' @return 
 #' @author fgochez
 #' @keywords
@@ -173,6 +274,7 @@ partitionLstSubproblems <- function(contents)
 	if( is.null(item)) NULL else as.numeric(gsub("[\\*[:space:]]+", "", item ))
 }
 
+# .nmMVOFNM
 
 #' Retrieves the NONMEM variable names in a set of strings
 #' @param txt vector of strings
@@ -188,12 +290,43 @@ partitionLstSubproblems <- function(contents)
 	txt
 }
 
-#' TODO: implement
-#' @param lstContents [C,+] character vector of lines of an "lst" file 
-#' @return A list of character vectors that has the list contents split up by problems
+#' Partitions the text of a report file into chunks of text for each "method" used.  This applies to
+#' NONMEM 7 reports only, as they will now have seperate parameter estimates, objective function values etc.
+#' for different $EST methods employed.
+#' @param lstProblemContents A character vector of lines of text from a NONMEM 7 report file  
+#' @return A list of character vectors of split text.  Each element of the list will also have an attribute
+#' called "method.name".  This will denote the actual name of the method used for that chunk. 
 #' @author fgochez
 
-partitionLstbyProblems <- function(lstContents)
+partitionMethods <- function(lstProblemContents)
 {
-	NULL
+	METHODDELIMITER <- "#METH"
+	# holds the indices of the lines where each METHOD result is held
+	methodLines <- grep(lstProblemContents, pattern = METHODDELIMITER)
+
+	if(length(methodLines) == 0)
+	{
+		RNMImportStop(msg = "No instances of #METH found in the report file!\n", match.call())
+		
+	}
+	# index of the last line of the report file
+	lastLine <- length(lstProblemContents)
+	# all of the points at which to partition
+	breakPoints <- c(methodLines, lastLine + 1)
+	# list that will hold each of the #METH chunks
+	methodBlocks <- vector(mode = "list", length = length(breakPoints) - 1 )
+	for(i in head(seq_along(breakPoints), -1 ))
+	{
+		
+		methodLine <- lstProblemContents[breakPoints[i]]
+		# assign the name of method as an attribute to the element of the list
+		# methodName <- pop( lstProblemContents[breakPoints[i]], option = "#METH", sep = ":", mode = "equal", inPlace = FALSE  )$op.out
+		ignoreMatch <- gregexpr( methodLine, pattern = "[[:space:]*]\\#METH:[[:space:]*]" )
+		
+		methodName <- substr(methodLine, start = 1 + attr(ignoreMatch[[1]], "match.length"), stop = nchar(methodLine))
+		
+		methodBlocks[[i]] <- lstProblemContents[breakPoints[i]:(breakPoints[i+1]-1)]
+		attr(methodBlocks[[i]], "method.name") <- methodName
+	}
+	methodBlocks
 }
