41da7852f65525bc0a9b98c925c34ba6019135de
fgochez <fgochez@mango-solutions.com>
2009-12-11 12:03:49 +0000

    Added tests for extracting objective function values for NONMEM 7 "basicmodel" type report files (related to issue 1860)
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@13982 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.importNmReport.R
+++ inst/unittests/runit.importNmReport.R
@@ -30,3 +30,38 @@ test.importNmReport.SimModel <- function()
 	checkEquals(names(probRes[[1]]), c("nRecords", "nIndividuals", "FinalEstimates"), "checking presence of correct elements in report" )
 	
 }
+
+# test import of a basic NONMEM 7 report
+
+test.importNmReport.BasicNM7 <- function()
+{
+	# load report1, which is from wexample1.lst
+	report1 <- importNmReport("wexample1.lst", 
+			path = file.path(unitTestPath, "testdata/wexample1"), version = "VII")
+	# TODO: check version...
+	probResult <- report1$problemResults[[1]]
+	checkEquals(probResult$nRecords, 500, " |Number of records is correct")
+	checkEquals(probResult$nIndividuals, 100, " |Number of individuals is correct")
+	
+	methodResults <- probResult$MethodResults
+	
+	objectiveFunValues <- sapply( methodResults, function(x) x$Objective.Final )
+	checkEquals( objectiveFunValues, -c(1120.307, 2477.443, 1145.163, 1121.028), msg = " |Objective function values retrieved correctly (check 1)" )
+	
+	# test 2, uses wexample2.lst
+	
+	report2 <- importNmReport("wexample2.lst", path = file.path(unitTestPath, "testdata/wexample2" ), version = "VII" )
+	methodResults2 <- report2$problemResults[[2]]$MethodResults
+	objectiveFunValues2 <- sapply( methodResults2, function(x) x$Objective.Final)
+	checkEquals( objectiveFunValues2, c( -19576.074, -10779.024 ,  -10772.143  ),  msg = " |Objective function values retrieved correctly (check 2)" )
+	
+	# test 3, uses wexample6.lst
+	
+	report3 <- importNmReport("wexample6.lst", path = file.path(unitTestPath, "testdata/wexample6" ), version = "VII" )
+	methodResult3 <- report3$problemResults[[1]]$MethodResults
+	objectiveFunValues3 <- sapply( methodResult3, function(x) x$Objective.Final)
+	
+	checkEquals( objectiveFunValues3, c(-4708.871, -4710.416 ), msg = " |Objective function values retrieved correctly (check 3)" )
+	
+	
+}
