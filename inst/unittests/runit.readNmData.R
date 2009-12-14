d0de22479aaa5ae102e22008bebdc55e71aa660e
fgochez <fgochez@mango-solutions.com>
2009-12-14 20:04:10 +0000

    Adds unit test for IGNORE=@. Addresses issue 1884.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14063 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.readNmData.R
+++ inst/unittests/runit.readNmData.R
@@ -20,4 +20,15 @@ test.readNmData <- function()
 	x.3 <-  readNmData(file.path(testDir, "data3"))
 	RNMImport:::.readNmData.nmSubset(x.3, nmCode = "(V2.EQ.12)", method = "ignore" )
 	checkEquals(x[-10,], x.3 )
+	
+	# check that ignore = "@" is handled correctly  - it should cause the entire header to be ignored
+	# along with other lines beginning with a letter
+	
+	x.4 <- as.matrix(readNmData(file.path(testDir, "data3.dat"), ignore = "@"))
+	rownames(x.4) <- NULL
+	checkEquals(colnames(x.4), c("V1", "V2", "V3"), msg = "| column names are ignored with IGNORE=@")
+	checkEquals(unname(x.4), unname(as.matrix(x.2)), msg = " |IGNORE=@ should ignore rows beginning with I, and hence should be equal to second test")
+	
+	
+	
 }
