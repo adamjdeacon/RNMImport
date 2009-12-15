fd1051f28897d24f6acc21d65b708b1aeb84ee70
fgochez <fgochez@mango-solutions.com>
2009-12-15 11:26:39 +0000

    Added code for importing report files for runs with simulation + model fitting.  Addresses issue 1889 and 1860.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14074 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/importNmLstNM7.R
+++ R/importNmLstNM7.R
@@ -75,9 +75,9 @@ importNmReport.NM7 <- function( fileName, path = NULL )
 		# simulation + model
 		if(simStep & objFun)
 		{	
-			RNMImportStop("Simulations + fitting problems for NONMEM 7 not yet imported")
+		#	RNMImportStop("Simulations + fitting problems for NONMEM 7 not yet imported")
 			logMessage(log = "stdReport", "Appears to be a simulation+modelling problem\n")
-			problemResults[[i]] <- importNmLstSimModel(currentProb, NA)
+			problemResults[[i]] <- importNmLstSimModel.NM7(currentProb, NA)
 		}
 		# only data simulation, no fit step
 		else if(simStep & !objFun)
@@ -105,7 +105,7 @@ importNmReport.NM7 <- function( fileName, path = NULL )
 
 .importNmLstBasicProb.NM7 <- function(contents)
 {
-	
+	# extract number of records and individuals
 	outList <- list() 
 	outList$nRecords     <- colonPop( contents, "TOT\\. NO\\. OF OBS RECS"   , inPlace = FALSE, numeric = TRUE )$op.out
 	outList$nIndividuals <- colonPop( contents, "TOT\\. NO\\. OF INDIVIDUALS", inPlace = FALSE, numeric = TRUE )$op.out
@@ -124,3 +124,159 @@ importNmReport.NM7 <- function( fileName, path = NULL )
 	outList
 	
 }
+
+#' Imports the contents of the report file of a SimModel type problem compiled with NONMEM 7
+#' @param [C,+] contents Contents of the report file for a single problem
+#' @param [N, 1] numSub Number of sub-problems in the problem 
+#' @title import NONMEM7 simulation+model fit problem report contents
+#' @return 
+#' @author fgochez
+#' @keywords
+
+importNmLstSimModel.NM7 <- function(contents, numSub = NA)
+{
+	contents <- cleanReportContents(contents)
+	if(is.na(numSub))
+	{
+		# find all lines of the form
+		#PROBLEM NO.:         1    SUBPROBLEM NO.:      N
+		subprobLines <- grep(contents, pattern = "PROBLEM NO\\.\\: [[:blank:]]*[0-9][[:blank:]]*SUBPROBLEM NO\\.\\:[[:blank:]]*[0-9]+[[:blank:]]*$")
+		# if there is only one sub-problem, then the above line will not appear, hence the need for the following
+		# logic 
+		numSub <- if(length(subprobLines) >= 1) length(subprobLines) else 1
+		
+	}
+#	if(numSub > 0)
+#	{
+	outList <- list()       
+	
+	# outList$VersionInfo  <- nmVersion( contents ) 
+	
+	# extract the number of records and individuals in the data
+	outList$nRecords     <- colonPop( contents, "TOT\\. NO\\. OF OBS RECS"   , inPlace = FALSE, numeric = TRUE )$op.out
+	outList$nIndividuals <- colonPop( contents, "TOT\\. NO\\. OF INDIVIDUALS", inPlace = FALSE, numeric = TRUE )$op.out
+	# split off the part of the control file that has the subproblems
+	# This line will look as follows (N is any integer greater than 1):
+	#PROBLEM NO.:         N     SUBPROBLEM NO.:      1
+	subprobStartLine <- 
+			grep(contents,
+					pattern = "PROBLEM NO\\.\\: [[:blank:]]*[0-9][[:blank:]]*SUBPROBLEM NO\\.\\:[[:blank:]]*1[[:blank:]]*$" )
+	if(length(subprobStartLine) == 0 || length(subprobStartLine) > 1 )
+		RNMImportStop("Not able to locate first subproblem in simulation lst file\n", call=match.call())
+	subprobContents <- tail(contents, -subprobStartLine + 1)
+	# cut the subproblems into chunks
+	subprobContents <- partitionLstSubproblems(subprobContents)
+	
+	# HACK ALERT
+	# the last subproblem does not terminate with a "1" section break for some reason.  This causes
+	# importing to fail for the last subproblem, so we "force" "1" to be the last line.
+	subprobContents[[length(subprobContents)]] <- c(subprobContents[[length(subprobContents)]], "1")	
+	# import the contents of the individual sub-problems
+	subprobStatements <- lapply(subprobContents, .importSubProbNM7)
+	
+	# get all objective function values for each method and sub-problem.  Structure these into a matrix
+	# with one row for each sub-problem, and one column for each method
+	
+	numMethods <- length(subprobStatements[[1]]$MethodResults)
+	numThetas <- length(subprobStatements[[1]]$MethodResults[[1]]$FinalEstimates$THETA)
+	omegaDim <- dim(subprobStatements[[1]]$MethodResults[[1]]$FinalEstimates$OMEGA)
+	
+	objectiveMatrix <- matrix(NA, nrow = numSub, ncol = numMethods)
+	
+	# thetas will be structured into a 3d arrays, with one row for for each method, 
+	# and one column for each theta,  
+	# and one sub-matrix for each simulation problems
+	
+	thetaArray <- array( NA, dim = c(numThetas, numMethods, numSub ) )
+	
+	# omegas and sigmas will be structured into lists of 3d arrays, with a submatrix for each method result, and one list
+	# element for each subproblem
+	omegaList <- vector(mode = "list", length = numSub )
+	sigmaList <- vector(mode = "list", length = numSub )
+	# extract all estimates and objective function values
+	
+	methodLabels <- paste("METHOD", 1:numMethods, sep = "")
+	for(i in seq_along(subprobStatements) )
+	{
+		objectiveFinals <- sapply(subprobStatements[[i]]$MethodResults, "[[", "Objective.Final" )
+		objectiveMatrix[i,] <- objectiveFinals
+		thetaFinals <- t(sapply(subprobStatements[[i]]$MethodResults, 
+						function(x) x$FinalEstimates$THETA))
+		thetaArray[,,i] <- thetaFinals
+		
+		omegaFinals <- lapply(subprobStatements[[i]]$MethodResults, function(x) x$FinalEstimates$OMEGA)
+		omegaFinals <- arrayFromMatrixList(omegaFinals, methodLabels)
+		sigmaFinals <- lapply(subprobStatements[[i]]$MethodResults, function(x) x$FinalEstimates$SIGMA)
+		sigmaFinals <- arrayFromMatrixList(sigmaFinals, methodLabels)
+		
+		sigmaList[[i]] <- sigmaFinals
+		omegaList[[i]] <- omegaFinals
+	}
+	
+	dimnames(objectiveMatrix) <- list(paste("sim", 1:numSub, sep = ""), paste("METH", seq_along(subprobStatements[[1]]$MethodResults), sep ="") )
+	
+	# now insert the thetas into a matrix with one row for each subproblem. 
+	# thetas <- t(sapply(subprobStatements, function(x) x$THETA))
+	
+	simLabels <- paste("sim", sep = "", 1:numSub)
+	
+	dimnames(thetaArray) <- list(methodLabels, names(subprobStatements[[1]]$MethodResults[[1]]$FinalEstimates$THETA)  ,simLabels)
+	# give the list elements the names of the simulations from which they came
+	names(omegaList) <- simLabels
+	names(sigmaList) <- simLabels
+	
+	outList$FinalEstimates <- list(THETA = thetaArray, 
+			OMEGA = omegaList, SIGMA = sigmaList, Objective.Minimum = objectiveMatrix)
+	
+#	}
+#	else
+#	{
+#		# if there is only one subproblem, then the file looks virtually identical to a basic model list file
+#		outList <- .importNmLstBasicProbNM7(contents)
+#		
+#		numThetas <- length(outList$MethodResults[[1]]$FinalEstimates$THETA)
+#		
+#		# however,we need to change the dimensions of some returned data
+#		# outList$FinalEstimates$THETA <- matrix(outList$FinalEstimates$THETA, 
+#		# 		nrow = 1, dimnames = list("sim1", names(outList$FinalEstimates$THETA)))
+#		thetaFinals <- t(sapply(outList$MethodResults, 
+#						function(x) x$FinalEstimates$THETA))
+#		thetaArray <- array()
+#		
+#		# outList$FinalEstimates$THETA <-
+#		
+#		x <- outList$FinalEstimates$OMEGA
+#		dim(x) <- c(dim(x), 1)
+#		dimnames(x) <- c(dimnames(outList$FinalEstimates$OMEGA), list("sim1"))
+#		outList$FinalEstimates$OMEGA <- x
+#		
+#		x <- outList$FinalEstimates$SIGMA
+#		dim(x) <- c(dim(x), 1)
+#		dimnames(x) <- c(dimnames(outList$FinalEstimates$SIGMA), list("sim1"))
+#		outList$FinalEstimates$SIGMA <- x
+#		
+#		
+#		names(outList$Objective.Minimum) <- "sim1"
+#		
+#	}
+	outList	
+}
+
+# internal function meant to be used internally by importNmLstSimModel ONLY
+.importSubProbNM7 <- function(txt)
+{
+	outList <- list() 
+	methodBlocks <- partitionMethods(txt)
+	methodResults <- lapply( methodBlocks, .importMethodBlock)
+	outList$MethodResults <- methodResults
+	### Find the sections of the list file
+	# lstList <- sectionLst( contents )
+	
+
+	
+	### Extract iteration information
+	# outList$Iter <- .importNmLstIter( lstList[["MONITORING OF SEARCH"]])
+	
+	outList
+}
+
