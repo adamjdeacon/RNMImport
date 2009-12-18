db8da980e1274e300c0e9490b888b7f5aed1b5fc
fgochez <fgochez@mango-solutions.com>
2009-12-18 10:28:07 +0000

    improved test that no error is generated when trying to close a connection with allowFail = TRUE.  Addresses issue 1157.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14187 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.logging.R
+++ inst/unittests/runit.logging.R
@@ -13,16 +13,21 @@ test.logging <- function()
 	 checkEquals(as.character(z),  "Error in closeLogConnection(logName = \"warnings\", allowFail = FALSE) : \n  Unable to close the connection warnings \n\n"
 					, msg = " correct error message when trying to close log without sucess" )
 	
-	closeLogConnection("warnings", TRUE)
+	z <- try(closeLogConnection("warnings", TRUE))
+	# adding a check that this is not an error.  This is not strictly needed because RUnit will report an error if there is one, but is included
+	# because it is cleaner.  Also related to issue 1157.
+	checkTrue(is.na(z), msg = " | Attempting to close a connection with allowFail = TRUE does not generate an error")
+	
 	# failure is now allowed without generating an exception, so check if the warning is true
 	# TODO: the following looks like it should work, but does not for some reason.  Fix it.
 	# checkEquals(names(last.warning)[1],  "Unable to close the connection warnings , but proceeding anyway \n" )
-			
+	
+	# try to direct a log to a file, output to it, then check		
 	setLogFile("stdReport", file.path(unitTestPath, "testdata/testlog.txt"))
 	logMessage("test", "stdReport")
 	checkEquals("test", scan(file.path(unitTestPath, "testdata/testlog.txt"), what = ""))
 	closeLogConnection("stdReport")
 	checkTrue(is.na(logConnection("stdReport")))
-	setLogConnection("stdReport", stdout())
 	file.remove(file.path(unitTestPath, "testdata/testlog.txt"))
+	setLogConnection("stdReport", stdout())
 }
