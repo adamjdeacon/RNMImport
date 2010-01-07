31ca014b60d79c35797e435685f15526bfa4ce6b
fgochez <fgochez@mango-solutions.com>
2010-01-07 15:22:31 +0000

    Updated getOmegas and getSigmas methods for NMBasicModel according to the updated design.  Partially issue 1955.  Changes to method for NMSimModel pending.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14436 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/retrieveParameters.R
+++ R/retrieveParameters.R
@@ -1,3 +1,11 @@
+# $LastChangedDate: $
+# $Rev: $
+# $LastChangedBy: $
+
+# parameter-related items that can be extracted
+
+PARAMITEMS <- c("final", "initial", "stderrors")
+
 #' Extracts theta estimates from a NONMEM object
 #' @param obj An object of class NMBasicModel, NMRun or NMSimModel
 #' @param stdErrors A boolean value that specifies whether standard errors should be returned
@@ -13,7 +21,7 @@ getThetas <- function(obj, stdErrors = FALSE, initial = FALSE, ...)
 }
 setGeneric("getThetas")
 
-getThetas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE)
+getThetas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE, returnMode = c("standard", "alternate"))
 {
 	thetas <- obj@thetaFinal
 	numRow <- nrow(thetas)
@@ -30,10 +38,10 @@ getThetas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE)
 }
 setMethod("getThetas", signature(obj = "NMBasicModel"), getThetas.NMBasicModel)
 
-getThetas.NMRun <- function(obj, stdErrors = FALSE, initial = FALSE, problemNum = 1)
+getThetas.NMRun <- function(obj, stdErrors = FALSE, initial = FALSE, problemNum = 1, returnMode = c("standard", "alternate"))
 {
 	dat <- getProblem(obj, problemNum)
-	thetas <- getThetas(dat, stdErrors, initial)
+	thetas <- getThetas(dat, stdErrors, initial, returnMode = returnMode)
 	thetas	
 }
 setMethod("getThetas", signature(obj = "NMRun"), getThetas.NMRun)
@@ -57,50 +65,88 @@ setMethod("getThetas", signature(obj = "NMSimModel"), getThetas.NMSimModel)
 #' @param stdErrors A boolean value that specifies whether standard errors should be returned
 #' @param initial A boolean value that specifies whether initial values of omega should be returned
 #' @param ... Additional arguments that apply to different classes. These are problemNum which specifies the run required for NMRun
+#' @param returnMode (NMRun or NMBasicProblem) Modifies how results are returned.  If "standard", a 3d structure is always returned
 #'    		  and subProblemNum which specifies the simulation required for NMSimModel
 #' @return A 3 dimensional array with a matrix of estimates, and if specified, a matrix of initial values and standard errors
 #' @author Mango Solutions <rweeks@mango-solutions.com>
 
-getOmegas <- function(obj, stdErrors = FALSE, initial = FALSE, ...)
+getOmegas <- function(obj, what = "final", ...)
 {
 	RNMImportStop(msg = "This method is not implemented for this class!")
 }
 setGeneric("getOmegas")
 
-getOmegas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE)
+getOmegas.NMBasicModel <- function(obj, what = "final")
 {
+	validWhat <- intersect(what, PARAMITEMS)
+	invalidWhat <- setdiff(what, PARAMITEMS)
+	
+	if(length(invalidWhat)) RNMImportWarning("Invalid items chosen:" %pst% paste(invalidWhat, collapse = ","))
+	
 	omegas <- obj@omegaFinal
-	thirdDimension <- dim(omegas)[3] 
-	if(!stdErrors)
-		res <- omegas[,,"estimates", drop = FALSE]
+	
+	# check if the matrices are one-by-one.  If they are, then we will have to coerce to a matrix later on
+	
+	oneByOne <- all(dim(omegas)[1:2] == c(1,1) )
+	finalEstimates <- omegas[,,"estimates", drop = TRUE]
+	if(oneByOne) finalEstimates <- matrix(finalEstimates, dimnames = dimnames(omegas)[1:2])
+	if("standardErrors" %in% dimnames(omegas)[[3]])
+	{
+		stdErrors <- omegas[,,"standardErrors", drop = TRUE]
+		if(oneByOne) stdErrors <- matrix(stdErrors, dimnames = dimnames(omegas)[1:2])
+	}
+	initialValues <- obj@omegaInitial
+	if(oneByOne) initialValues <- matrix(initialValues, dimnames = dimnames(omegas)[1:2])
+	# no valid option selected, thrown an error
+	if(length(validWhat) == 0) RNMImportStop("No valid items selected for retrieval!", call = match.call())
+	if(length(validWhat) == 1)
+	{
+		res <- switch(validWhat, 
+				"final" = finalEstimates,
+				# TODO: if these are length 0, generate an error?
+				"initial" = initialValues,
+				"stderrors" = {
+					if(is.null(stdErrors))
+						RNMImportStop("Standard errors not available \n", call = match.call())
+					stdErrors
+				}
+				)
+		# this occurs if the omegas were a 1x1 matrix to begin with.  We wish to force the returned value to be a matrix	
+	} # end if length(validWhat) == 1
 	else
-		res <- omegas
-	if(initial)
-		attr(res, "Initial") <- obj@omegaInitial
-	if(stdErrors && thirdDimension == 1 )
-		RNMImportWarning(msg = "No standard errors are available!")
-	res	
+	{
+		res <- list()
+		# TODO: check for missing initial values?
+		if("initial" %in% validWhat) res$initial.estimates <- initialValues
+		if("final" %in% validWhat) res$final.estimates <- finalEstimates
+		if("stderrors" %in% validWhat) 
+		{
+			if(is.null(stdErrors)) RNMImportWarning("Standard errors not available \n")
+			else res$standard.errors <- stdErrors
+		}
+	}
+	res
 }
 setMethod("getOmegas", signature(obj = "NMBasicModel"), getOmegas.NMBasicModel)
 
-getOmegas.NMRun <- function(obj, stdErrors = FALSE, initial = FALSE, problemNum = 1)
+getOmegas.NMRun <- function(obj, what = "final", problemNum = 1)
 {
 	dat <- getProblem(obj, problemNum)
-	omegas <- getOmegas(dat, stdErrors, initial)
+	omegas <- getOmegas(dat, what = what)
 	omegas
 }
 setMethod("getOmegas", signature(obj = "NMRun"), getOmegas.NMRun)
 
-getOmegas.NMSimModel <- function(obj, stdErrors = FALSE, initial = FALSE, subProblemNum = 1)
+getOmegas.NMSimModel <- function(obj, what = "final", subProblemNum = 1)
 {
-	if(stdErrors)
-		RNMImportWarning(msg = "No standard errors are available!")
+#	if(stdErrors)
+#		RNMImportWarning(msg = "No standard errors are available!")
 	numSimulations <- obj@numSimulations
 	if(any(!(subProblemNum %in% 1:numSimulations)))
 		RNMImportStop(msg = "Subproblem number is not valid!")	
 	omegas <- obj@omegaFinal[, , subProblemNum, drop = FALSE]
-	if(initial)
-		attr(omegas, "Initial") <- obj@omegaInitial
+#	if(initial)
+		#attr(omegas, "Initial") <- obj@omegaInitial
 	omegas
 }
 setMethod("getOmegas", signature(obj = "NMSimModel"), getOmegas.NMSimModel)
@@ -114,46 +160,88 @@ setMethod("getOmegas", signature(obj = "NMSimModel"), getOmegas.NMSimModel)
 #' @return A 3 dimensional array with a matrix of estimates, and if specified, a matrix of initial values and standard errors
 #' @author Mango Solutions <rweeks@mango-solutions.com>
 
-getSigmas <- function(obj, stdErrors = FALSE, initial = FALSE, ...)
+getSigmas <- function(obj, what = "final", ...)
 {
 	RNMImportStop(msg = "This method is not implemented for this class!")
 }
 setGeneric("getSigmas")
 
-getSigmas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE)
+getSigmas.NMBasicModel <- function(obj, what = "final")
 {
+	validWhat <- intersect(what, PARAMITEMS)
+	invalidWhat <- setdiff(what, PARAMITEMS)
+	
+	if(length(invalidWhat)) RNMImportWarning("Invalid items chosen:" %pst% paste(invalidWhat, collapse = ","))
+	
 	sigmas <- obj@sigmaFinal
-	thirdDimension <- dim(sigmas)[3] 
-	if(!stdErrors)
-		res <- sigmas[,,"estimates", drop = FALSE]
+	oneByOne <- all(dim(sigmas)[1:2] == c(1,1) )
+	finalEstimates <- sigmas[,,"estimates", drop = TRUE]
+	if(oneByOne) finalEstimates <- matrix(finalEstimates, dimnames = dimnames(sigmas)[1:2])
+	if("standardErrors" %in% dimnames(sigmas)[[3]]) 
+	{
+		stdErrors <- sigmas[,,"standardErrors", drop = TRUE]
+		if(oneByOne) stdErrors <- matrix(stdErrors, dimnames = dimnames(sigmas)[1:2])
+	}
+	
+	initialValues <- obj@sigmaInitial
+	if(oneByOne) initialValues <- matrix(initialValues, dimnames = dimnames(sigmas)[1:2])
+	
+	# no valid option selected, thrown an error
+	if(length(validWhat) == 0) RNMImportStop("No valid items selected for retrieval!", call = match.call())
+	if(length(validWhat) == 1)
+	{
+		res <- switch(validWhat, 
+				"final" = finalEstimates,
+				# TODO: if these are length 0, generate an error?
+				"initial" = initialValues,
+				"stderrors" = {
+					if(is.null(stdErrors))
+						RNMImportStop("Standard errors not available \n", call = match.call())
+					stdErrors
+				}
+		)
+		# this occurs if the sigmas were a 1x1 matrix to begin with.  We wish to force the returned value to be a matrix
+		if(is.null(dim(res))) 
+		{ 
+			dim(res) <- c(1,1)
+			dimnames(res) <- dimnames(sigmas)[1:2]	
+		}	
+	} # end if length(validWhat) == 1
 	else
-		res <- sigmas
-	if(initial)
-		attr(res, "Initial") <- obj@sigmaInitial
-	if(stdErrors && thirdDimension == 1 )
-		RNMImportWarning(msg = "No standard errors are available!")
+	{
+		res <- list()
+		# TODO: check for missing initial values?
+		if("initial" %in% validWhat) res$initial.estimates <- initialValues
+		if("final" %in% validWhat) res$final.estimates <- finalEstimates
+		if("stderrors" %in% validWhat) 
+		{
+			if(is.null(stdErrors)) RNMImportWarning("Standard errors not available \n")
+			else res$standard.errors <- stdErrors
+		}
+	}
 	res
 }
 setMethod("getSigmas", signature(obj = "NMBasicModel"), getSigmas.NMBasicModel)
 
-getSigmas.NMRun <- function(obj, stdErrors = FALSE, initial = FALSE, problemNum = 1)
+getSigmas.NMRun <- function(obj, what = "final", problemNum = 1)
 {
 	dat <- getProblem(obj, problemNum)
-	sigmas <- getSigmas(dat, stdErrors, initial)
+	sigmas <- getSigmas(dat, what = what)
 	sigmas
 }
 setMethod("getSigmas", signature(obj = "NMRun"), getSigmas.NMRun)
 
-getSigmas.NMSimModel <- function(obj, stdErrors = FALSE, initial = FALSE, subProblemNum = 1)
+getSigmas.NMSimModel <- function(obj, what = "final", subProblemNum = 1)
 {
-	if(stdErrors)
-		RNMImportWarning(msg = "No standard errors are available!")
+#	if(stdErrors)
+#		RNMImportWarning(msg = "No standard errors are available!")
 	numSimulations <- obj@numSimulations
 	if(any(!(subProblemNum %in% 1:numSimulations)))
 		RNMImportStop(msg = "Subproblem number is not valid!")	
 	sigmas <- obj@sigmaFinal[, , subProblemNum, drop = FALSE]
-	if(initial)
-		attr(sigmas, "Initial") <- obj@sigmaInitial
+#	if(initial)
+#		attr(sigmas, "Initial") <- obj@sigmaInitial
 	sigmas
 }
+
 setMethod("getSigmas", signature(obj = "NMSimModel"), getSigmas.NMSimModel)
