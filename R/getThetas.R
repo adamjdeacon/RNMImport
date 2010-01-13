7487ba238688ed1c072e84a29bdda798bfc3530e
fgochez <fgochez@mango-solutions.com>
2010-01-13 14:17:55 +0000

    Changed the way getThetas works to be more consistent with the changes made to getOmegas and getSigmas.  Related to issue 1974.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14654 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/getThetas.R
+++ R/getThetas.R
@@ -1,6 +1,6 @@
-# $LastChangedDate: $
-# $Rev: $
-# $LastChangedBy: $
+# $LastChangedDate$
+# $Rev$
+# $LastChangedBy$
 
 # TODO: reduce amount of copy-paste code through refactoring
 
@@ -17,74 +17,146 @@ PARAMITEMS <- c("final", "initial", "stderrors")
 #' @return A matrix of named rows for final estimates, initial estimates, standard errors etc. as applicable
 #' @author rweeks
 
-getThetas <- function(obj, stdErrors = FALSE, initial = FALSE, ...)
+getThetas <- function(obj, what = "final", ...)
 {
 	RNMImportStop(msg = "This method is not implemented for this class!")
 }
 setGeneric("getThetas")
 
-getThetas.NMBasicModel <- function(obj, stdErrors = FALSE, initial = FALSE, returnMode = c("standard", "alternate"),...)
+getThetas.NMBasicModel <- function(obj, what = "final",...)
 {
+	validWhat <- intersect(what, PARAMITEMS)
+	invalidWhat <- setdiff(what, PARAMITEMS)
+	
+	if(length(invalidWhat)) RNMImportWarning("Invalid items chosen:" %pst% paste(invalidWhat, collapse = ","))
+	
 	thetas <- obj@thetaFinal
 	numRow <- nrow(thetas)
-	if(!stdErrors)
-		res <- thetas["estimates",, drop = FALSE]
+	
+	finalEstimates <- thetas["estimates",, drop = TRUE]
+
+	if("standardErrors" %in% dimnames(thetas)[[1]])
+		stdErrors <- thetas["standardErrors",, drop = TRUE]
+	else stdErrors <- NULL
+	initialValues <- obj@thetaInitial
+	
+	if(length(validWhat) == 0) RNMImportStop("No valid items selected for retrieval!", call = match.call())
+	
+	if(length(validWhat) == 1)
+	{
+		res <- switch(validWhat, 
+				"final" = finalEstimates,
+				# TODO: if these are length 0, generate an error?
+				"initial" = initialValues,
+				"stderrors" = {
+					if(is.null(stdErrors))
+						RNMImportStop("Standard errors not available \n", call = match.call())
+					stdErrors
+				}
+		)
+		# this occurs if the omegas were a 1x1 matrix to begin with.  We wish to force the returned value to be a matrix	
+	} # end if length(validWhat) == 1
 	else
-		res <- thetas
-	if(initial)
-		attr(res, "Initial") <-  obj@thetaInitial
-	if(stdErrors && !("standardErrors" %in% rownames(thetas)))
-		RNMImportWarning(msg = "No standard errors are available!")
+	{
+		res <- matrix(ncol = ncol(thetas), nrow = 0, dimnames = list(NULL, colnames(thetas)))
+		if("initial" %in% validWhat) res <- rbind(res, initialValues )
+		if("final" %in% validWhat) res <- rbind(res, "estimates" = finalEstimates)
+		if( "stderrors" %in% validWhat )
+		{
+			if(is.null(stdErrors)) RNMImportWarning("Standard errors not available \n")
+			else res <- rbind(res, "standardErrors" = stdErrors)
+		}
+	}
 	res
 	
 }
 
 setMethod("getThetas", signature(obj = "NMBasicModel"), getThetas.NMBasicModel)
 
-getThetas.NMRun <- function(obj, stdErrors = FALSE, initial = FALSE, problemNum = 1, returnMode = c("standard", "alternate"), method = 1)
+getThetas.NMRun <- function(obj, what = "final", problemNum = 1, method = 1)
 {
 	dat <- getProblem(obj, problemNum)
-	thetas <- getThetas(dat, stdErrors, initial, returnMode = returnMode, method = method)
+	thetas <- getThetas(dat, what = what, method = method)
 	thetas	
 }
 setMethod("getThetas", signature(obj = "NMRun"), getThetas.NMRun)
 
-getThetas.NMBasicModelNM7 <- function(obj, stdErrors = FALSE, initial = FALSE, returnMode = c("standard", "alternate"),
-			method = 1,...)
+getThetas.NMBasicModelNM7 <- function(obj, what = "final",	method = 1,...)
 {
+	validWhat <- intersect(what, PARAMITEMS)
+	invalidWhat <- setdiff(what, PARAMITEMS)
+	
+	if(length(invalidWhat)) RNMImportWarning("Invalid items chosen:" %pst% paste(invalidWhat, collapse = ","))
+	
 	methodChosen <- .selectMethod(obj@methodNames, method)
 	thetas <- obj@thetaFinal[[methodChosen]]
-	numRow <- nrow(thetas)
-	if(!stdErrors)
-		res <- obj@thetaFinal[[methodChosen]]
-	else 
+	
+	finalEstimates <- thetas 
+	stdErrors <- obj@thetaStderr[[methodChosen]]
+
+	initialValues <- obj@thetaInitial
+	
+	if(length(validWhat) == 0) RNMImportStop("No valid items selected for retrieval!", call = match.call())
+	
+	if(length(validWhat) == 1)
 	{
-		stdErr <- obj@thetaStderr[[methodChosen]]
-		if(is.null(stdErr))
+		res <- switch(validWhat, 
+				"final" = finalEstimates,
+				# TODO: if these are length 0, generate an error?
+				"initial" = initialValues,
+				"stderrors" = {
+					if(is.null(stdErrors))
+						RNMImportStop("Standard errors not available \n", call = match.call())
+					stdErrors
+				}
+		)
+		# this occurs if the omegas were a 1x1 matrix to begin with.  We wish to force the returned value to be a matrix	
+	} # end if length(validWhat) == 1
+	else
+	{
+		res <- matrix(ncol = length(thetas), nrow = 0, dimnames = list(NULL, names(thetas)))
+		if("initial" %in% validWhat) res <- rbind(res, initialValues )
+		if("final" %in% validWhat) res <- rbind(res, "estimates" = finalEstimates)
+		if( "stderrors" %in% validWhat )
 		{
-			RNMImportWarning(msg = "No standard errors are available!")
-			res <- obj@thetaFinal[[methodChosen]]
+			if(is.null(stdErrors)) RNMImportWarning("Standard errors not available \n")
+			else res <- rbind(res, "standardErrors" = stdErrors)
 		}
-		else res <- rbind("estimates" = obj@thetaFinal[[methodChosen]], "standardErrors" = stdErr)
 	}
-	if(initial)
-		attr(res, "Initial") <-  obj@thetaInitial
 	res
 	
 }
 
 setMethod("getThetas", signature(obj = "NMBasicModelNM7"), getThetas.NMBasicModelNM7)
 
-getThetas.NMSimModel <- function(obj, stdErrors = FALSE, initial = FALSE, subProblemNum = 1)
+getThetas.NMSimModel <- function(obj, what = "final", subProblemNum = 1, ...)
 {
-	if(stdErrors)
+	
+	validWhat <- intersect(what, PARAMITEMS)
+	invalidWhat <- setdiff(what, PARAMITEMS)
+	if("stderrors" %in% validWhat)
 		RNMImportWarning(msg = "No standard errors are available!")
+	
 	numSimulations <- obj@numSimulations
 	if(any(!(subProblemNum %in% 1:numSimulations)))
 		RNMImportStop(msg = "Subproblem number is not valid!")	
-	thetas <- obj@thetaFinal[subProblemNum,]
-	if(initial)
-		attr(thetas, "Initial") <- obj@thetaInitial	
-	thetas
+	
+	finalEstimates <- obj@thetaFinal[subProblemNum,]
+	
+	initial <- obj@thetaInitial
+	
+	if(length(validWhat) == 1)
+	{
+		res <- switch(validWhat, 
+				"final" = finalEstimates,
+				# TODO: if these are length 0, generate an error?
+				"initial" = initial
+		)
+		# this occurs if the omegas were a 1x1 matrix to begin with.  We wish to force the returned value to be a matrix	
+	} # end if length(validWhat) == 1
+	else
+		res <- list("initial.estimates" = initial, "final.estimates"  = finalEstimates)
+	
+	res
 }
 setMethod("getThetas", signature(obj = "NMSimModel"), getThetas.NMSimModel)
