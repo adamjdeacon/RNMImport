3f5e194bd379a87237a352a21ec55325a1e01454
fgochez <fgochez@mango-solutions.com>
2010-01-13 14:43:01 +0000

    Updated tests due to issue 1974 and issue 1955
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14659 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.retrieveParameters.R
+++ inst/unittests/runit.retrieveParameters.R
@@ -1,3 +1,6 @@
+# $LastChangedBy$
+# $LastChangedDate$
+# $Rev$
 # TODO: Add comment
 # 
 # Author: rweeks
@@ -12,14 +15,18 @@ test.getThetas <- function()
 			path = testDir1)
 	prob1 <- getProblem(run1)
 	
-	expThetas <- matrix(c(19.6, 84.6, 1.66), 1, 3, dimnames = list(c("estimates"), c("THETA1", "THETA2", "THETA3")))
-	expAttributes <- matrix(c(-Inf, 18.7, Inf, -Inf, 87.3, Inf, -Inf, 2.13, Inf), 3,
+	# expThetas <- matrix(c(19.6, 84.6, 1.66), 1, 3, dimnames = list(c("estimates"), c("THETA1", "THETA2", "THETA3")))
+	expThetas <- c("THETA1" = 19.6, "THETA2" = 84.6, "THETA3" = 1.66)
+	expThetaInitial <- matrix(c(-Inf, 18.7, Inf, -Inf, 87.3, Inf, -Inf, 2.13, Inf), 3,
 						dimnames = list(c("lowerBound", "initial", "upperBound"), c("THETA1", "THETA2", "THETA3")))
 	
 	checkEquals(getThetas(prob1), expThetas)
 	checkEquals(getThetas(run1), expThetas)
-	checkEquals(attr(getThetas(prob1, initial = TRUE), "Initial"),  expAttributes)
-	checkEquals(attr(getThetas(run1, initial = TRUE), "Initial"),  expAttributes)
+	
+	checkEquals(getThetas(prob1, what = c("final", "initial")), rbind( expThetaInitial, "estimates" = expThetas))
+	
+	
+	# checkEquals(attr(getThetas(run1, initial = TRUE), "Initial"),  expAttributes)
 	
 	#Check NMSimModel
 	testDir2 <- file.path(unitTestPath, "testdata/TestSimRun")
@@ -29,11 +36,11 @@ test.getThetas <- function()
 	
 	simThetas <- matrix(c(17.2, 18.3, 18.0, 19.0, 17.1, 117.0, 108.0, 108.0, 98.9, 109.0, 1.24, 1.40, 1.24, 1.55, 1.38), 5, 3)
 	dimnames(simThetas) <- list(c("sim1", "sim2", "sim3", "sim4", "sim5"), c("THETA1", "THETA2", "THETA3"))
-	attrSim <- c(19.6, 84.6, 1.66)
-	names(attrSim) <- c("THETA1", "THETA2", "THETA3")
+	simInitial <- c(19.6, 84.6, 1.66)
+	names(simInitial) <- c("THETA1", "THETA2", "THETA3")
 	
 	checkEquals(getThetas(prob2, subProblemNum = 1:5),  simThetas)
-	checkEquals(attr(getThetas(prob2, initial = TRUE, subProblemNum = 1:5), "Initial"),  attrSim)
+	# checkEquals(attr(getThetas(prob2, initial = TRUE, subProblemNum = 1:5), "Initial"),  attrSim)
 }
 
 test.getOmegas <- function()
@@ -72,11 +79,11 @@ test.getOmegas <- function()
 	simOmegas[, , 1] <- diag(c(0.174, 0.19, 1.45)); simOmegas[, , 2] <- diag(c(0.167, 0.143, 1.24))
 	simOmegas[, , 3] <- diag(c(0.181, 0.142, 1.57)); simOmegas[, , 4] <- diag(c(0.245, 0.189, 0.945))
 	simOmegas[, , 5] <- diag(c(0.174, 0.197, 1.47))
-	attrOm <- diag(c(0.164, 0.165, 1.3)) 
-	dimnames(attrOm) <- list(c("OMEGA1", "OMEGA2", "OMEGA3"), c("OMEGA1", "OMEGA2", "OMEGA3"))
+	initialSimOmegas <- diag(c(0.164, 0.165, 1.3)) 
+	dimnames(initialSimOmegas) <- list(c("OMEGA1", "OMEGA2", "OMEGA3"), c("OMEGA1", "OMEGA2", "OMEGA3"))
 	
 	checkEquals(getOmegas(prob2, subProblemNum = 1:5),  simOmegas)
-	# checkEquals(attr(getOmegas(prob2, initial = TRUE, subProblemNum = 1:5), "Initial"),  attrOm)
+	checkEquals(getOmegas(prob2, what = "initial", subProblemNum = 1:5),  initialSimOmegas)
 }
 
 test.getSigmas <- function()
@@ -102,8 +109,8 @@ test.getSigmas <- function()
 	
 	simSigmas <- array(c(0.0241, 0.026, 0.0262, 0.0295, 0.0244), dim = c(1,1,5), 
 			dimnames = list("SIGMA1", "SIGMA1", c("sim1", "sim2", "sim3", "sim4", "sim5")))
-	attrSig <- matrix(0.0202, 1, dimnames = list("SIGMA1", "SIGMA1"))
+	sigmaInitial <- matrix(0.0202, 1, dimnames = list("SIGMA1", "SIGMA1"))
 	
 	checkEquals(getSigmas(prob2, subProblemNum = 1:5),  simSigmas)
-	# checkEquals(attr(getSigmas(prob2, initial = TRUE, subProblemNum = 1:5), "Initial"),  attrSig)
+	checkEquals(getSigmas(prob2, what = "initial", subProblemNum = 1:5), sigmaInitial)
 }
