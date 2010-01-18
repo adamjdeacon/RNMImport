1101ed09827a5dedc6ad2233db8fee1c19434715
fgochez <fgochez@mango-solutions.com>
2010-01-18 10:38:02 +0000

    Refactored to speed up, in accordance with issue 1983.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14748 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.retrieveParameters.R
+++ inst/unittests/runit.retrieveParameters.R
@@ -6,49 +6,74 @@
 # Author: rweeks
 ###############################################################################
 
+# tests getThetas to ensure that  behaviour under various combinations of parameters works as expected
+# note that this function is also tested in runit.importNm.R, so this focuses more on
+# parameter combinations than anything else
+
 
 test.getThetas <- function()
 {
 	#Check NMBasicModel and NMRun
-	testDir1 <- file.path(unitTestPath, "testdata/TestRun")
-	run1 <- importNm(conFile = "testdata1.ctl", reportFile = "testdata1.lst", 
-			path = testDir1)
+	
+	testRuns <- RNMImport:::getInternalTestRuns()
+	run1 <- testRuns[["NMBasic"]]
+	
 	prob1 <- getProblem(run1)
 	
+	run3 <- testRuns[["NMBasicNotab"]]
 	# expThetas <- matrix(c(19.6, 84.6, 1.66), 1, 3, dimnames = list(c("estimates"), c("THETA1", "THETA2", "THETA3")))
 	expThetas <- c("THETA1" = 19.6, "THETA2" = 84.6, "THETA3" = 1.66)
 	expThetaInitial <- matrix(c(-Inf, 18.7, Inf, -Inf, 87.3, Inf, -Inf, 2.13, Inf), 3,
 						dimnames = list(c("lowerBound", "initial", "upperBound"), c("THETA1", "THETA2", "THETA3")))
-	
+	expThetaStderrs <-  c("THETA1" = 0.963, "THETA2" = 3.56, "THETA3" = 0.218)
 	checkEquals(getThetas(prob1), expThetas)
 	checkEquals(getThetas(run1), expThetas)
 	
-	checkEquals(getThetas(prob1, what = c("final", "initial")), rbind( expThetaInitial, "estimates" = expThetas))
+	# final and initial together
 	
+	checkEquals(getThetas(prob1, what = c("final", "initial")), rbind( expThetaInitial, "estimates" = expThetas),
+				msg = " |what = final and initial works correctly")
 	
-	# checkEquals(attr(getThetas(run1, initial = TRUE), "Initial"),  expAttributes)
+	tryStderrs <- try(getThetas(run1, what = "stderrors"), silent = TRUE)
+	checkTrue(length(grep(tryStderrs, pattern = "Standard errors not available")) > 0, 
+			msg = " | correct error message when no standard errors available")
 	
-	#Check NMSimModel
+	checkEquals(getThetas(run3, what = c("final", "stderrors")), 
+			rbind("estimates" = expThetas, "standardErrors" = expThetaStderrs), 
+			msg = " | final and stderrs together is correct" )
+	
+	############ Check NMSimModel
+	
 	testDir2 <- file.path(unitTestPath, "testdata/TestSimRun")
-	run2 <- importNm(conFile = "testdata1sim.con", reportFile = "testdata1sim.lst", 
-			path = testDir2)
+	
+	run2 <- testRuns[["NMSimMod"]]
+	
 	prob2 <- getProblem(run2)
 	
 	simThetas <- matrix(c(17.2, 18.3, 18.0, 19.0, 17.1, 117.0, 108.0, 108.0, 98.9, 109.0, 1.24, 1.40, 1.24, 1.55, 1.38), 5, 3)
 	dimnames(simThetas) <- list(c("sim1", "sim2", "sim3", "sim4", "sim5"), c("THETA1", "THETA2", "THETA3"))
 	simInitial <- c(19.6, 84.6, 1.66)
+	
 	names(simInitial) <- c("THETA1", "THETA2", "THETA3")
 	
-	checkEquals(getThetas(prob2, subProblemNum = 1:5),  simThetas)
-	# checkEquals(attr(getThetas(prob2, initial = TRUE, subProblemNum = 1:5), "Initial"),  attrSim)
+	checkEquals(getThetas(prob2, subProblemNum = 1:5),  simThetas, msg = " |for sim model, final as expected")
+	
+	checkEquals(getThetas(prob2, subProblemNum = 1:5, what = c("final", "initial")),  
+			list("initial.estimates" = simInitial, "final.estimates" = simThetas ), 
+			msg = " |for sim model, final + initial as expected")
+	
+	######## NONMEM 7 run
+	
 }
 
 test.getOmegas <- function()
 {
-	#Check NMBasicModel and NMRun
+	########### Check NMBasicModel and NMRun
 	testDir1 <- file.path(unitTestPath, "testdata/TestRun")
-	run1 <- importNm(conFile = "testdata1.ctl", reportFile = "testdata1.lst", 
-			path = testDir1)
+	
+	testRuns <- RNMImport:::getInternalTestRuns()
+	run1 <- testRuns[["NMBasic"]]
+	
 	prob1 <- getProblem(run1)
 	
 	expOmegas <- array(c(0.164, 0, 0, 0, 0.165, 0, 0, 0, 1.3), c(3,3))
@@ -67,10 +92,18 @@ test.getOmegas <- function()
 	checkEquals(omegaTest1,  expOmegaInit)
 	checkEquals(omegaTest2,  expOmegaInit)	
 	
+	run3 <- importNm(conFile = "testdata1notab.ctl", 
+			reportFile = "testdata1notab.lst", 
+			path = file.path(unitTestPath,"testdata/testrunnotab"))
+	
+	
 	#Check NMSimModel
-	testDir2 <- file.path(unitTestPath, "testdata/TestSimRun")
-	run2 <- importNm(conFile = "testdata1sim.con", reportFile = "testdata1sim.lst", 
-			path = testDir2)
+	
+	
+	# run2 <- importNm(conFile = "testdata1sim.con", reportFile = "testdata1sim.lst", 
+	#		path = testDir2)
+	
+	run2 <- testRuns[["NMSimMod"]]
 	prob2 <- getProblem(run2)
 	
 	simOmegas <- array(dim = c(3,3,5), dimnames = list(paste("OMEGA", 1:3, sep = ""), 
@@ -80,10 +113,14 @@ test.getOmegas <- function()
 	simOmegas[, , 3] <- diag(c(0.181, 0.142, 1.57)); simOmegas[, , 4] <- diag(c(0.245, 0.189, 0.945))
 	simOmegas[, , 5] <- diag(c(0.174, 0.197, 1.47))
 	initialSimOmegas <- diag(c(0.164, 0.165, 1.3)) 
+	
 	dimnames(initialSimOmegas) <- list(c("OMEGA1", "OMEGA2", "OMEGA3"), c("OMEGA1", "OMEGA2", "OMEGA3"))
 	
 	checkEquals(getOmegas(prob2, subProblemNum = 1:5),  simOmegas)
 	checkEquals(getOmegas(prob2, what = "initial", subProblemNum = 1:5),  initialSimOmegas)
+	
+	checkEquals(getOmegas(prob2, what = c("final", "initial"), subProblemNum = 1:5), 
+			list("initial.estimates" = initialSimOmegas , "final.estimates" = simOmegas))
 }
 
 test.getSigmas <- function()
