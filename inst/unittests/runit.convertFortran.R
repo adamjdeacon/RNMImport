d61eb980bd56007ecf1610f17464b04eae23b13e
fgochez <fgochez@mango-solutions.com>
2010-01-19 10:48:38 +0000

    Tests for the convertFortran95Ops function.  Related to issue 1980.
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14790 4c17f647-02f2-7e4c-97ba-2b8bca437fff

---
+++ inst/unittests/runit.convertFortran.R
@@ -0,0 +1,30 @@
+# $LastChangedDate$
+# $LastChangedBy$
+# $Rev$
+# 
+# Author: fgochez
+###############################################################################
+
+
+# test the convertFortran95Ops function
+
+test.convertFortran <- function()
+{
+	codeBlock1 <- "ID/=1.AND.TIME>0"
+	
+	checkEquals(RNMImport:::convertFortran95Ops(codeBlock1), "ID.NE.1.AND.TIME.GT.0")
+	
+	codeBlock2 <- "AMT==250.OR.AMT==260"
+	
+	checkEquals(RNMImport:::convertFortran95Ops(codeBlock2), "AMT.EQ.250.OR.AMT.EQ.260")
+	
+	codeBlock3 <- "TIME<=100"
+	checkEquals(RNMImport:::convertFortran95Ops(codeBlock3), "TIME.LE.100")
+	
+	codeBlock4 <- "X>200.AND.Y>=1000"
+	checkEquals(RNMImport:::convertFortran95Ops(codeBlock4), "X.GT.200.AND.Y.GE.1000")
+	
+	codeBlock5 <- "X<200.AND.Y<=1000"
+	checkEquals(RNMImport:::convertFortran95Ops(codeBlock5) , "X.LT.200.AND.Y.LE.1000")
+	
+}
