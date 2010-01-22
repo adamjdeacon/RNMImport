e979096e4ace80b37f3980eff32bc0ef1c643fe2
fgochez <fgochez@mango-solutions.com>
2010-01-22 11:18:45 +0000

    Added show methods for NONMEM 7 objects (issue 1978 )
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14939 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/show_methods.R
+++ R/show_methods.R
@@ -50,6 +50,7 @@ show.NMBasicModel <- function(object)
 				}
 				cat("Error:\n")
 				print(Error)
+			
 				cat("Parameter estimates:\n###############\n")
 				cat("THETAs:\n")
 				print(object@thetaFinal["estimates",])
@@ -74,6 +75,66 @@ show.NMBasicModel <- function(object)
 }
 
 setMethod("show", signature(object = "NMBasicModel"), show.NMBasicModel)
+
+show.NMBasicModelNM7 <- function(object)
+{
+	controlStatements <- object@controlStatements
+	
+	cat("Standard NONMEM (7) problem:\n###############\n")
+	
+	with( controlStatements, 
+			{
+				
+				cat("Problem statement: ", Problem, "\n")
+				
+				cat("Data file:", Data[,"File"], "\n")
+				
+				cat("Input table dimensions:\n")
+				cat(dim(object@inputData), "\n")
+				cat("Input table columns:\n")
+				cat(colnames(object@outputData), "\n")
+				
+				if(!is.null(controlStatements$PRED))
+				{		
+					cat("PRED:\n")
+					print(PRED)
+					cat("\n")
+				}
+				if(!is.null(controlStatements$PK)) 
+				{
+					cat("PK: \n")
+					print(PK)
+					cat("\n")
+				}
+				cat("Error:\n")
+				print(Error)
+				cat("Methods used: \n")
+				print(object@methodNames)
+				cat("Parameter estimates:\n###############\n")
+				cat("THETAs:\n")
+				print(object@thetaFinal)
+				cat("OMEGAs:\n")
+				print(object@omegaFinal)
+				cat("SIGMAs:\n")
+				print(object@sigmaFinal)
+				
+				if(all(dim(object@outputData) == c(0,0)))
+					cat("No output table data\n")
+				else 
+				if(class(object@outputData) != "list")
+				{
+					cat("Output table files: ")
+					cat(paste(Tables[,"File"], collapse = ","), "\n")
+					cat("Output table dimensions:\n")
+					cat(dim(object@outputData), "\n")
+					cat("Output table columns:\n")
+					cat(colnames(object@outputData), "\n")
+				}
+			} ) # end with(controlStatements ...
+}
+
+setMethod("show", signature(object = "NMBasicModelNM7"), show.NMBasicModelNM7)
+
 
 show.NMSimDataGen <- function(object)
 {
@@ -164,3 +225,58 @@ show.NMSimModel <- function(object)
 }
 
 setMethod("show", signature(object = "NMSimModel"), show.NMSimModel)
+
+
+show.NMSimModelNM7 <- function(object)
+{
+	controlStatements <- object@controlStatements
+	with(controlStatements, 
+			{
+				
+				cat("NONMEM (7) data simulation problem:\n###############\n")
+				cat("Problem statement: ", Problem, "\n")
+				if(!is.null(controlStatements$PRED))
+				{		
+					cat("PRED:\n")
+					print(PRED)
+					cat("\n")
+				}
+				if(!is.null(controlStatements$PK)) 
+				{
+					cat("PK: \n")
+					print(PK)
+					cat("\n")
+				}
+				cat("Data file:", Data[,"File"], "\n")
+				cat("Input table dimensions:\n")
+				cat(dim(object@inputData), "\n")
+				cat("Input table columns:\n")
+				cat(colnames(object@inputData), "\n")
+				
+				cat("Output table dimensions:\n")
+				cat(dim(object@outputData), "\n")
+				
+				cat("Output table columns:\n")
+				cat(colnames(object@outputData), "\n")
+				
+				cat("Number of simulations performed: ", Sim["nSub"], "\n")
+				
+				cat("Seeds: ", na.omit(object@seeds), "\n" )
+				cat("Methods used: \n")
+				print(object@methodNames)
+				# now display estimates
+				cat("Final estimates for each subproblem:\n\n")
+				
+				cat("THETAs:\n")
+				print(object@thetaFinal)
+				cat("OMEGAs:\n")
+				print(object@omegaFinal)
+				cat("SIGMAs:\n")
+				print(object@sigmaFinal)
+				
+				cat("Objective function(s): \n")
+				print(object@objectiveFinal)
+			})
+}
+
+setMethod("show", signature(object = "NMSimModelNM7"), show.NMSimModelNM7)
