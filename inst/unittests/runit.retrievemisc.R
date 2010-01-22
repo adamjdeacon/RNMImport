54e24f9a85fdc9db456da657ccec99e3c5e85ac7
fgochez <fgochez@mango-solutions.com>
2010-01-22 11:26:57 +0000

    refactored so that runs are not re-loaded from disk.  Also added tests with addMinInfo set to TRUE and FALSE, and addMinInfo for NONMEM 7 basic model (issue 1977)
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@14942 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.retrievemisc.R
+++ inst/unittests/runit.retrievemisc.R
@@ -4,20 +4,45 @@
 
 test.getObjective <- function()
 {
+	testRuns <- RNMImport:::getInternalTestRuns()
+	
 	setNmPath("internalunit",  file.path(unitTestPath, "testdata/TestRun") )
-	run1 <- importNm(conFile = "testdata1.ctl", reportFile = "testdata1.lst", 
-			path = "(internalunit)")
-	
-	obj <- getObjective(run1, addMinInfo = FALSE)
-	
-	checkEquals(obj, 3228.988)
-	checkEquals(getObjective(getProblem(run1), addMinInfo = FALSE), 3228.988 )
+	run1 <- testRuns$NMBasic
+	# NMBasicModel
+
+	checkEquals(getObjective(getProblem(run1), addMinInfo = FALSE), 3228.988 ) 
 	
 	obj <- getObjective(run1, addMinInfo = TRUE)
 	target <- 3228.988
 	attr(target, "minInfo") <- c("minResult" = "SUCCESSFUL", "numEval" = "143", 
 			"numSigDigits" = "3.5")
 	checkEquals(obj, target)
+	
+	# NMSimModel
+	
+	run2 <- testRuns$NMSimMod
+	prob2 <- getProblem(run2)
+	checkEquals(getObjective(run2, subProblems = 1:5), getObjective(prob2, subProblems = 1:5))
+	checkEquals(getObjective(prob2, subProblems = c(2, 4)), 
+			structure(c(3575.252, 3606.526), .Names = c("sim2","sim4")),
+			msg = " |objective functions for simulation problem are correct")
+	
+	# NMBasicModelNM7
+	# TODO: complete this:
+	# run3 <- importNm( conFile = "TestData1.ctl", path = path = file.path(unitTestPath, "testdata/Test"), reportFile = "testdata1SIM.LST"  )
+	
+	# NMSimModelNM7
+	
+	run3 <- testRuns$NMBasicNM7 
+	prob3 <- getProblem(run3)
+	
+	checkEquals(getObjective(run3, method = 1:2), getObjective(prob3, method = 1:2), msg = " |getobjective the same on run and NMBasicModelNM7" )
+	
+	objTarget <- c(3335.250, 2339.093)
+	attr(objTarget, "minInfo") <- c("OPTIMIZATION COMPLETED", "STOCHASTIC PORTION COMPLETED")
+	checkEquals(getObjective(run3, method = 1:2), objTarget)
+	checkEquals(getObjective(prob3, method = 1, addMinInfo = FALSE), 3335.250 )
+	checkEquals(getObjective(prob3, method = 2, addMinInfo = FALSE), 2339.093 )
 	
 	removeNmPath("internalunit")
 	
@@ -29,10 +54,10 @@ test.getObjective <- function()
 
 test.getMisc <- function()
 {
-	run1 <- importNm(conFile = "testdata1notab.ctl", reportFile = "testdata1notab.lst", 
-			path = file.path(unitTestPath, "testdata/TestRunNoTab"))
-	run2 <- importNm(conFile = "TestData1SIM.con", reportFile = "TestData1SIM.lst", 
-			path = file.path(unitTestPath, "testdata/TestSimRun"))
+	testRuns <- RNMImport:::getInternalTestRuns()
+	
+	run1 <- testRuns$NMBasicNotab 
+	run2 <- testRuns$NMSimMod	
 	
 	checkEquals( getNmVersion(run1), c(major = "VI", "minor" = "2" ) , " version of run1 is correct")
 	checkEquals( getNmVersion(run2), c(major = "VI", "minor" = "1" ) , " version of run2 is correct")
