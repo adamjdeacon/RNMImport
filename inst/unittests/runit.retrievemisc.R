e771138b3cb1e70c7fb2dffe2c025c911440b91e
fgochez <fgochez@mango-solutions.com>
2010-01-27 16:40:55 +0000

    Tests that a NULL is returned correctly by getEstimateCov when invoked on NMBasicModel with no covariance matrix (tests issue 2048)
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@15154 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.retrievemisc.R
+++ inst/unittests/runit.retrievemisc.R
@@ -28,10 +28,6 @@ test.getObjective <- function()
 			msg = " |objective functions for simulation problem are correct")
 	
 	# NMBasicModelNM7
-	# TODO: complete this:
-	# run3 <- importNm( conFile = "TestData1.ctl", path = path = file.path(unitTestPath, "testdata/Test"), reportFile = "testdata1SIM.LST"  )
-	
-	# NMSimModelNM7
 	
 	run3 <- testRuns$NMBasicNM7 
 	prob3 <- getProblem(run3)
@@ -46,13 +42,66 @@ test.getObjective <- function()
 	
 	removeNmPath("internalunit")
 	
+	# NMSimModelNM7
+	
+}
+
+# test get estimate cov
+
+test.getEstimateCov <- function()
+{
+	testRuns <- RNMImport:::getInternalTestRuns()
+	
+	run1 <- testRuns$NMBasicNotab
+	prob1 <- getProblem(run1)
+	
+	checkEquals(getEstimateCov(run1, corMatrix = TRUE) ,getEstimateCov(run1, corMatrix = TRUE))
+	# expected covariance matrices
+	expCovMat <- structure(c(0.927, 2.16, 0.0347, 0.00617, 0, 0, 0.00252, 0, 0.0471, 
+					0.00176, 2.16, 12.7, 0.18, 0.00892, 0, 0, 0.00903, 0, 0.39, 0.000474, 
+					0.0347, 0.18, 0.0474, -0.000661, 0, 0, -0.000648, 0, 0.0657, 
+					0.000168, 0.00617, 0.00892, -0.000661, 0.000571, 0, 0, 0.000373, 
+					0, -0.000778, 1.59e-05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+					0, 0, 0, 0, 0, 0, 0, 0.00252, 0.00903, -0.000648, 0.000373, 0, 
+					0, 0.000486, 0, -0.000812, 6.95e-06, 0, 0, 0, 0, 0, 0, 0, 0, 
+					0, 0, 0.0471, 0.39, 0.0657, -0.000778, 0, 0, -0.000812, 0, 0.119, 
+					0.000239, 0.00176, 0.000474, 0.000168, 1.59e-05, 0, 0, 6.95e-06, 
+					0, 0.000239, 1.04e-05), .Dim = c(10L, 10L), .Dimnames = list(
+					c("TH1", "TH2", "TH3", "OM11", "OM12", "OM13", "OM22", "OM23", 
+							"OM33", "SG11"), c("TH1", "TH2", "TH3", "OM11", "OM12", "OM13", 
+							"OM22", "OM23", "OM33", "SG11")))
+	
+	expCorMat <- structure(c(1, 0.629, 0.166, 0.268, 0, 0, 0.119, 0, 0.142, 0.567, 
+					0.629, 1, 0.232, 0.105, 0, 0, 0.115, 0, 0.317, 0.0413, 0.166, 
+					0.232, 1, -0.127, 0, 0, -0.135, 0, 0.874, 0.24, 0.268, 0.105, 
+					-0.127, 1, 0, 0, 0.708, 0, -0.0943, 0.206, 0, 0, 0, 0, 0, 0, 
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0.119, 0.115, -0.135, 
+					0.708, 0, 0, 1, 0, -0.107, 0.0978, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+					0, 0.142, 0.317, 0.874, -0.0943, 0, 0, -0.107, 0, 1, 0.215, 0.567, 
+					0.0413, 0.24, 0.206, 0, 0, 0.0978, 0, 0.215, 1), .Dim = c(10L, 
+					10L), .Dimnames = list(c("TH1", "TH2", "TH3", "OM11", "OM12", 
+							"OM13", "OM22", "OM23", "OM33", "SG11"), c("TH1", "TH2", "TH3", 
+							"OM11", "OM12", "OM13", "OM22", "OM23", "OM33", "SG11")))
+	
+	checkEquals(getEstimateCov(prob1), expCovMat, msg = " |covariance matrix as expected")
+	test1 <- getEstimateCov(run1, corMatrix = TRUE)
+	
+	checkEquals(test1, list("covariance" = expCovMat, "correlation" = expCorMat),
+			msg = " | extracting with both")
+
+	# check for appropriate error handling
+	
+	run2 <- testRuns$NMBasic
+	
+	checkTrue(is.null(getEstimateCov(run2)), msg = " |NULL returned when no parameter covariance matrix found" )
+	
 }
 
 # tests the following functions:
 # getNmVersion
 #
 
-test.getMisc <- function()
+test.getNmVersion <- function()
 {
 	testRuns <- RNMImport:::getInternalTestRuns()
 	
