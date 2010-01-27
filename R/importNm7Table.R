f4a676c315e643beff318857db5e3146022a7032
fgochez <fgochez@mango-solutions.com>
2010-01-27 14:04:11 +0000

    Improved some comments, and now closing text connection after reading a table to avoid warning.  Related to implementation of issue 1979
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@15144 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/importNm7Table.R
+++ R/importNm7Table.R
@@ -9,7 +9,7 @@
 #' Imports one of the new files produced by NONMEM 7: .phi, .cor, .cov, etc. into a  list of data.frames.
 #' @param file Name of the file
 #' @param path Path to the file (including NONMEM path in brackets)
-#' @param type Type of the file: One of "cov", "ext", or "phi".  
+#' @param type Type of the file: One of "cov", "ext", or "phi".  "cov" should be used for .cor, .cov or .coi files.
 #' @title Import new NONMEM 7 files
 #' @return a list of data.frames holding the content of the individual sub-tables within each file.  Each
 #' table will have as an attribute "method", holding the name of the method associated to it
@@ -47,14 +47,18 @@ importNm7Tables <- function(file, path = "", type = c("cov", "ext", "phi"))
 				pattern = "^[[:space:]]+", replacement = "" )
 	}
 	# all file types are handled identically except for "phi" files, where the first column is not
-	
 	# treated as a set of row names
-	
 	if(type %in% c("phi", "ext")) row.names <- NULL else row.names <- 1
+	
+	# covariance matrices should be returned as matrices, not data.frames
+	if(type == "cov") coerceToMatrix <- TRUE else coerceToMatrix <- FALSE
 	# read actual contents into individual tables
 	tableContents <- lapply( seq_along(tableBodies), 
 			function(i) {
-				y <- read.table(textConnection(tableBodies[[i]]), header = TRUE, row.names = row.names)
+				tableTextConn <- textConnection(tableBodies[[i]])
+				y <- read.table(tableTextConn, header = TRUE, row.names = row.names, stringsAsFactors = FALSE)
+				close(tableTextConn)
+				if(coerceToMatrix) y <- as.matrix(y)
 				attr(y, "method") <- tableMethodNames[[i]]
 				y
 			})
