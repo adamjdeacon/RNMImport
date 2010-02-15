75122df593d27db8bf224b2e84cbc47852a0e78f
fgochez <fgochez@mango-solutions.com>
2010-02-15 22:59:58 +0000

    Added more test coverage of input/output data import
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@15512 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.importNm.R
+++ inst/unittests/runit.importNm.R
@@ -55,6 +55,42 @@ test.importNm.Basic <- function()
 	expSigma <- array(0.0202, c(1,1), dimnames = list("SIGMA1", "SIGMA1"))
 	checkEquals(getSigmas(run1, what = "final"), expSigma, msg = " |sigma final estimates imported as expected")
 	
+	## check import of data
+	
+	allData <- nmData(run1, dataType = c("input", "output"), returnMode ="DFList")
+	
+	inData <- allData$input
+	inDataTest <- as.matrix(rbind(head(inData, 2), tail(inData, 2)))
+	rownames(inDataTest) <- NULL
+	
+	checkEquals(inDataTest, structure(c(1, 1, 65, 65, 2, 2, 1, 1, 55, 55, 64, 64, 2, 2, 1, 
+							1, 154, 154, 180, 180, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 
+							1, 1, 1, 10000, 0, 0, 0, 80.97, 80.97, 99.79, 99.79, 0, 1, 10, 
+							12, 1, 1, 1.1, 1.1, 169.98, 114.57, 7.63, 6.15, 1, 0, 0, 0, 1, 
+							0, 0, 0, 12, 0, 0, 0, 1, 1, 65, 65, 0, 0, 0, 0), .Dim = c(4L, 
+							19L), .Dimnames = list(NULL, c("SID", "SEX", "AGE", "RACE", "HT", 
+									"SMOK", "HCTZ", "PROP", "CON", "AMT", "WT", "TIME", "SECR", "DV", 
+									"EVID", "SS", "II", "ID", "OCC"))), msg = " | input data correct")
+	
+	outData <- allData$output
+	
+	outDataTest <- as.matrix(rbind(head(outData, 2), tail(outData, 2)))
+	rownames(outDataTest) <- NULL
+	
+	checkEquals(outDataTest, structure(c(1, 1, 65, 65, 0, 1, 10, 12, 8.2032, 113.58, 8.7492, 
+		5.6597, 19.721, 0.0086953, -0.12792, 0.086636, 169.98, 114.57, 
+	7.63, 6.15, 9.0449, 89.979, 7.1931, 4.5225, 0, 24.591, 0.43694, 
+	1.6275, 0, 0.33079, -0.51811, 1.4561, 18.004, 18.004, 20.978, 
+	20.978, 72.62, 72.62, 95.784, 95.784, 2.5569, 2.5569, 0.774, 
+	0.774, 55, 55, 64, 64, 154, 154, 180, 180, 80.97, 80.97, 99.79, 
+	99.79, 1, 1, 1.1, 1.1, 2, 2, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 1, 
+	1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 65, 65, 0, 
+	0.33079, 0.51811, 1.4561), .Dim = c(4L, 24L), .Dimnames = list(
+	    NULL, c("ID", "TIME", "IPRED", "IWRES", "DV", "PRED", "RES", 
+	    "WRES", "CL", "V", "KA", "AGE", "HT", "WT", "SECR", "SEX", 
+	    "RACE", "SMOK", "HCTZ", "PROP", "CON", "OCC", "SID", "absWRES"
+	    ))), msg = " |output data correct")
+	
 	# check importNm works correctly with dropInputColumns = FALSE
 	run2 <- importNm(conFile = "testdata1.ctl", reportFile = "testdata1.lst", 
 				path = "(internalunit)", dropInputColumns = FALSE)
@@ -162,6 +198,42 @@ test.importNm.SimModel <- function()
 	checkEquals(getThetas(run4), getThetas(run2), msg = " | identical thetas")
 	
 	checkEquals(nmData(run4, dataType = "input"), nmData(run2, dataType = "input"))
+	
+	allData <- nmData(run2, dataType = c("input", "output"), returnMode ="DFList")
+	
+	inData <- allData$input
+	inDataTest <- as.matrix(rbind(head(inData, 2), tail(inData, 2)))
+	rownames(inDataTest) <- NULL
+	
+	checkEquals(inDataTest, structure(c(1, 1, 65, 65, 2, 2, 1, 1, 55, 55, 64, 64, 2, 2, 1, 
+							1, 154, 154, 180, 180, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 
+							1, 1, 1, 10000, 0, 0, 0, 80.97, 80.97, 99.79, 99.79, 0, 1, 10, 
+							12, 1, 1, 1.1, 1.1, 169.98, 114.57, 7.63, 6.15, 1, 0, 0, 0, 1, 
+							0, 0, 0, 12, 0, 0, 0, 1, 1, 65, 65, 0, 0, 0, 0), .Dim = c(4L, 
+							19L), .Dimnames = list(NULL, c("SID", "SEX", "AGE", "RACE", "HT", 
+									"SMOK", "HCTZ", "PROP", "CON", "AMT", "WT", "TIME", "SECR", "DV", 
+									"EVID", "SS", "II", "ID", "OCC"))), msg = " | input data correct")
+	
+	outData <- subset(allData$output, sel = -NSIM)
+	
+	outDataTest <- as.matrix(rbind(head(outData, 2), tail(outData, 2)))
+	rownames(outDataTest) <- NULL
+	
+	checkEquals(outDataTest, structure(c(1, 1, 65, 65, 0, 1, 10, 12, 18.908, 77.135, 28.666, 
+							23.214, 7.9898, -0.0031396, 0.19734, 0.070985, 169.98, 76.893, 
+							34.323, 24.862, 20.136, 73.154, 12.678, 9.2604, 0, 3.7388, 21.645, 
+							15.601, 0, 0.063253, 3.6048, 1.9752, 16.414, 16.414, 9.1826, 
+							9.1826, 99.378, 99.378, 87.059, 87.059, 1.1048, 1.1048, 3.7522, 
+							3.7522, 55, 55, 64, 64, 154, 154, 180, 180, 80.97, 80.97, 99.79, 
+							99.79, 1, 1, 1.1, 1.1, 2, 2, 1, 1, 2, 2, 1, 1, 0, 0, 0, 0, 1, 
+							1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 65, 65, 0, 
+							0.063253, 3.6048, 1.9752), .Dim = c(4L, 24L), .Dimnames = list(
+							NULL, c("ID", "TIME", "IPRED", "IWRES", "DV", "PRED", "RES", 
+									"WRES", "CL", "V", "KA", "AGE", "HT", "WT", "SECR", "SEX", 
+									"RACE", "SMOK", "HCTZ", "PROP", "CON", "OCC", "SID", "absWRES"
+							))), msg = " |output data correct" )
+	
+	
 }
 
 # import standard NONMEM 7 run
@@ -176,7 +248,7 @@ test.importNm.BasicNM7 <- function()
 	allThetas <- sapply(as.numeric(1:2), function(i) getThetas(run1, method = i, what = "final"))
 	allOmegas <- lapply(as.numeric(1:2), function(i) getOmegas(run1, method = i, what = "final"))
 	allSigmas <- lapply(as.numeric(1:2), function(i) getSigmas(run1, method = i, what = "final"))
-#	
+	# FIXME: Theta standard errors not checked!
 	thetaStderr <- getThetas(run1, method = 1, what = "stderrors")
 	omegaStderr <- getOmegas(run1, method = 1, what = "stderrors")
 	sigmaStderr <- getSigmas(run1, method = 1, what = "stderrors") 
