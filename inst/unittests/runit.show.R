1a7e6c55fc2a8769069dd5858ca46b5d6ffcfe9f
fgochez <fgochez@mango-solutions.com>
2010-02-16 00:27:02 +0000

    Restored tests.  They ONLY work from the Rgui when executed manually via runRNMImportTests.  Addresses issue 2176
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@15513 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- inst/unittests/runit.show.R
+++ inst/unittests/runit.show.R
@@ -2,7 +2,11 @@
 # Author: fgochez
 ###############################################################################
 
-# currently out of date.  These printed output depends on file paths which can depend on the r installation
+# NOTE:
+# These tests only appear to work if executed from inside the Rgui using runRNMImportTests(...)
+# they DO NOT work when run from ant test (thus Hudson), because the output dumped by the show methods
+# there can have a different number of lines.
+# TODO: Find some way around this.
 
 .test.show.NMRun <- function()
 {
@@ -25,7 +29,7 @@
 
 }
 
-.test.show.NMBasicModel <- function()
+test.show.NMBasicModel <- function()
 {
 	testRuns <- RNMImport:::getInternalTestRuns()
 	
@@ -34,26 +38,25 @@
 	prob1 <- getProblem(run1)
 	test1 <- capture.output(show(prob1))
 	
-	checkEquals(test1,
+	expectedOut <- 
 			c("Standard NONMEM problem:", "###############", "Problem statement:  System Test 1 ", 
 					"Data file: Data1 ", "Input table dimensions:", "1061 19 ", "Input table columns:", 
-					"ID TIME IPRED IWRES DV PRED RES WRES CL V KA AGE HT WT SECR SEX RACE SMOK HCTZ PROP CON OCC SID absWRES ", 
-					"PK: ", "[1] \"TVCL=THETA(1) \"        \"TVV=THETA(2) \"         \"TVKA=THETA(3) \"       ", 
-					"[4] \"CL=TVCL*EXP(ETA(1) ) \" \"V =TVV *EXP(ETA(2) ) \" \"KA=TVKA*EXP(ETA(3) ) \"", 
-					"[7] \"S2=V\"                 ", "", "Error:", "[1] \"IPRED = F\"                 \"IRES  = DV - F\"           ", 
-					"[3] \"W     = F\"                 \"IF(W.EQ.0) W = 1\"         ", 
-					"[5] \"IWRES = IRES/W\"            \"Y     = IPRED + W*EPS(1) \"", 
-					"Parameter estimates:", "###############", "THETAs:", "THETA1 THETA2 THETA3 ", 
-					" 19.60  84.60   1.66 ", "OMEGAs:", "       OMEGA1 OMEGA2 OMEGA3", 
-					"OMEGA1  0.164  0.000    0.0", "OMEGA2  0.000  0.165    0.0", 
-					"OMEGA3  0.000  0.000    1.3", "SIGMAs:", "[1] 0.0202", "Output table files: sdtab1,patab1,cotab1,catab1,mutab1,mytab1 ", 
+					"SID SEX AGE RACE HT SMOK HCTZ PROP CON AMT WT TIME SECR DV EVID SS II ID OCC ", 
+					"PK: ", "[1] \"TVCL=THETA(1) \"        \"TVV=THETA(2) \"         \"TVKA=THETA(3) \"        \"CL=TVCL*EXP(ETA(1) ) \" \"V =TVV *EXP(ETA(2) ) \" \"KA=TVKA*EXP(ETA(3) ) \"", 
+					"[7] \"S2=V\"                 ", "", "Error:", "[1] \"IPRED = F\"                 \"IRES  = DV - F\"            \"W     = F\"                 \"IF(W.EQ.0) W = 1\"          \"IWRES = IRES/W\"           ", 
+					"[6] \"Y     = IPRED + W*EPS(1) \"", "Parameter estimates:", 
+					"###############", "THETAs:", "THETA1 THETA2 THETA3 ", " 19.60  84.60   1.66 ", 
+					"OMEGAs:", "       OMEGA1 OMEGA2 OMEGA3", "OMEGA1  0.164  0.000    0.0", 
+					"OMEGA2  0.000  0.165    0.0", "OMEGA3  0.000  0.000    1.3", 
+					"SIGMAs:", "[1] 0.0202", "Output table files: sdtab1,patab1,cotab1,catab1,mutab1,mytab1 ", 
 					"Output table dimensions:", "1061 24 ", "Output table columns:", 
-					"ID TIME IPRED IWRES DV PRED RES WRES CL V KA AGE HT WT SECR SEX RACE SMOK HCTZ PROP CON OCC SID absWRES ")
-		)
+					"ID TIME IPRED IWRES DV PRED RES WRES CL V KA AGE HT WT SECR SEX RACE SMOK HCTZ PROP CON OCC SID absWRES "
+			)
+	checkEquals(test1,	expectedOut, msg = " |show for NMBasicModel correct")
 	
 }
 
-.test.show.NMSimModel <- function()
+test.show.NMSimModel <- function()
 {
 	testRuns <- RNMImport:::getInternalTestRuns()
 	
@@ -62,11 +65,11 @@
 	prob1 <- getProblem(run1)
 	test1 <- capture.output(show(prob1))
 	
-	test1 <- test1[ - grep(test1, pattern = "[[:space:]]*")]
 	expected1 <- c("NONMEM data simulation problem:", "###############", "Problem statement:  System Test 2 ", 
 			"PK: ", "[1] \"TVCL=THETA(1) \"        \"TVV=THETA(2) \"         \"TVKA=THETA(3) \"        \"CL=TVCL*EXP(ETA(1) ) \" \"V =TVV *EXP(ETA(2) ) \" \"KA=TVKA*EXP(ETA(3) ) \"", 
 			"[7] \"S2=V\"                 ", "", "Data file: Data1 ", "Input table dimensions:", 
 			"1061 19 ", "Input table columns:", "SID SEX AGE RACE HT SMOK HCTZ PROP CON AMT WT TIME SECR DV EVID SS II ID OCC ", 
+			"Output table files: sdtab1,patab1,cotab1,catab1,mutab1,mytab1 ", 
 			"Output table dimensions:", "5305 24 ", "Output table columns:", 
 			"ID TIME IPRED IWRES DV PRED RES WRES CL V KA AGE HT WT SECR SEX RACE SMOK HCTZ PROP CON OCC SID absWRES ", 
 			"Number of simulations performed:  5 ", "Seeds:  20050213 ", 
@@ -89,7 +92,37 @@
 			"SIGMA1 0.0295", "", ", , sim5", "", "       SIGMA1", "SIGMA1 0.0244", 
 			"", "Objective function(s): ", "    sim1     sim2     sim3     sim4     sim5 ", 
 			"3696.247 3575.252 3660.355 3606.526 3701.472 ")
-	expected1 <- expected1[ - grep(expected1, pattern = "[[:space:]]*") ]
-	checkEquals(test1, expected1)
+	writeLines(test1,"showtest2.txt")
+	checkEquals(test1, expected1, msg = " | show for sim model correct\n")
+	
+}
 
+test.show.NMBasicNM7 <- function()
+{
+	testRuns <- RNMImport:::getInternalTestRuns()
+	
+	run1 <- testRuns[["NMBasicNM7"]]
+	
+	prob1 <- getProblem(run1)
+	test1 <- capture.output(show(prob1))
+	
+	expected1 <- c("Standard NONMEM (7) problem:", "###############", "Problem statement:  NM7 test 1 ", 
+			"Data file: Data1 ", "Input table dimensions:", "1061 19 ", "Input table columns:", 
+			"SID SEX AGE RACE HT SMOK HCTZ PROP CON AMT WT TIME SECR DV EVID SS II ID OCC ", 
+			"PK: ", "[1] \"TVCL=THETA(1) \"        \"TVV=THETA(2) \"         \"TVKA=THETA(3) \"        \"CL=TVCL*EXP(ETA(1) ) \" \"V =TVV *EXP(ETA(2) ) \" \"KA=TVKA*EXP(ETA(3) ) \"", 
+			"[7] \"S2=V\"                 ", "", "Error:", "[1] \"IPRED = F\"                 \"IRES  = DV - F\"            \"W     = F\"                 \"IF(W.EQ.0) W = 1\"          \"IWRES = IRES/W\"           ", 
+			"[6] \"Y     = IPRED + W*EPS(1) \"", "Methods used: ", "[1] \"Iterative Two Stage\"                               \"Stochastic Approximation Expectation-Maximization\"", 
+			"Parameter estimates:", "###############", "THETAs:", "[[1]]", 
+			"  TH1   TH2   TH3 ", "20.00 77.30  1.27 ", "", "[[2]]", "  TH1   TH2   TH3 ", 
+			"19.10 76.70  1.68 ", "", "OMEGAs:", "[[1]]", "      ETA1  ETA2  ETA3", 
+			"ETA1 0.157 0.000 0.000", "ETA2 0.000 0.162 0.000", "ETA3 0.000 0.000 0.737", 
+			"", "[[2]]", "      ETA1  ETA2 ETA3", "ETA1 0.145 0.000 0.00", 
+			"ETA2 0.000 0.149 0.00", "ETA3 0.000 0.000 1.42", "", "SIGMAs:", 
+			"[[1]]", "       EPS1", "EPS1 0.0259", "", "[[2]]", "       EPS1", 
+			"EPS1 0.0266", "", "Output table files: tab1 ", "Output table dimensions:", 
+			"1061 11 ", "Output table columns:", "ID TIME IPRED IWRES WT ETA1 DV PRED RES WRES absWRES "
+	)
+	
+	checkEquals(test1, expected1, msg = " | show for NMBasic NM7 correct")
+	
 }
