15517d25e1579ffe89e369f737df2a6b91caeaf0
fgochez <fgochez@mango-solutions.com>
2010-04-26 19:21:44 +0000

    TA121 : Added parameter "tableTitles", which allows one to import table sets that don't have titles (e.g. "Table No. ....")
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@17196 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/importNm7Table.R
+++ R/importNm7Table.R
@@ -10,16 +10,23 @@
 #' @param file Name of the file
 #' @param path Path to the file (including NONMEM path in brackets)
 #' @param type Type of the file: One of "cov", "ext", or "phi".  "cov" should be used for .cor, .cov or .coi files.
+#' @param tableTitles [L,1] Are the table titles present in the file?  Should be FALSE, if, for example, one generates an EXT
+#' file using the NOTITLE=1 option.  Currently this option ONLY works when type = "ext"
 #' @title Import new NONMEM 7 files
 #' @return a list of data.frames holding the content of the individual sub-tables within each file.  Each
 #' table will have as an attribute "method", holding the name of the method associated to it
 #' @author fgochez
 #' @export
 
-importNm7Tables <- function(file, path = ".", type = c("cov", "ext", "phi"))
+importNm7Tables <- function(file, path = ".", type = c("cov", "ext", "phi"), tableTitles = TRUE)
 {
 	type <- match.arg(type)
 	filePath <- processPath(path)
+	
+	# tableTitles will not apply to other function types for now
+	
+	if(type != "ext") tableTitles <- TRUE
+	
 	# regular expression that indicates where the tables start
 	
 	TABLEDELIMETER <- "^[[:space:]]*TABLE NO.[[:space:]]*[0-9]+"
@@ -30,15 +37,25 @@ importNm7Tables <- function(file, path = ".", type = c("cov", "ext", "phi"))
 	if(inherits(fileContents, "try-error")) RNMImportStop( "Unable to load file" )
 	
 	# determine where tables begin and end
-	
-	tableBoundaries <- grep(fileContents, pattern = TABLEDELIMETER )
+	if(tableTitles)
+		tableBoundaries <- grep(fileContents, pattern = TABLEDELIMETER )
+	else
+		tableBoundaries <- grep(fileContents, pattern = "ITERATION")
 	if(length(tableBoundaries) == 0) RNMImportStop( "No table delimeters found in the file : not a valid covariance file?" )
 	
 	else if(length(tableBoundaries) == 1) tableList <- list(fileContents[tableBoundaries:length(fileContents)])
 	else tableList <- splitVector(fileContents, tableBoundaries, includeEnd = TRUE)
+	if(tableTitles)
+	{
+		tableHeadings <- sapply(tableList, function(x) x[1])
+		tableBodies <- lapply(tableList, function(x) tail(x, -1))
+	}
+	else
+	{
+		tableHeadings <- NULL
+		tableBodies <- tableList
+	}
 	
-	tableHeadings <- sapply(tableList, function(x) x[1])
-	tableBodies <- lapply(tableList, function(x) tail(x, -1))
 	tableMethodNames <- vector(mode = "character", length = length(tableHeadings))
 	
 	for(i in seq_along(tableHeadings))
@@ -59,7 +76,9 @@ importNm7Tables <- function(file, path = ".", type = c("cov", "ext", "phi"))
 				y <- read.table(tableTextConn, header = TRUE, row.names = row.names, stringsAsFactors = FALSE)
 				close(tableTextConn)
 				if(coerceToMatrix) y <- as.matrix(y)
-				attr(y, "method") <- tableMethodNames[[i]]
+				# method names will be missing if there are no titles
+				if(length(tableMethodNames) > 0)
+					attr(y, "method") <- tableMethodNames[[i]]
 				y
 			})
 	
