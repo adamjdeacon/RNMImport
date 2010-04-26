1cc4403c2a340897c1479c6c540491a17191578d
fgochez <fgochez@mango-solutions.com>
2010-04-26 19:18:13 +0000

    TA121 : Routine for importing NONMEM 7 iterations using control file statements
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@17194 4c17f647-02f2-7e4c-97ba-2b8bca437fff

---
+++ R/importNM7Iterations.R
@@ -0,0 +1,37 @@
+# $LastChangedDate$
+# $LastChangedBy$
+# $Rev$
+# 
+# Author: fgochez
+###############################################################################
+
+#' 
+#' @param files 
+#' @param noTitles 
+#' @param noLabels 
+#' @title Import NONMEM7 parameter iterations from .EXT files
+#' @return 
+#' @author fgochez
+
+importNm7Iterations <- function( files = NULL, noTitles = NULL, noLabels = NULL, methods = NULL, path = "." )
+{
+	numFiles = length(files)
+	if(numFiles == 0) return(list())
+	
+	RNMImportStopifnot(all(c(length(noTitles) == numFiles, length(noLabels) == numFiles, 
+					length(methods) == numFiles)), 
+			msg = "Inconsistent length of vector parameters!", match.call())
+	
+	RNMImportStopifnot( all(noLabels == "0") 
+	, msg = "NOLABELS=1 option currently disallowed", match.call())
+	
+	iterations <- vector(mode = "list", length = numFiles)
+	for(i in seq_along(files))
+	{
+		if(files[i] == "") next
+		iterations[[i]] <- importNm7Tables(files[i], type = "ext", tableTitles = noTitles[i] == "0" , path = path)
+		names(iterations[[i]]) <- sapply(iterations[[i]], function(x) attr(x, "method"))
+	}
+	
+	do.call(c, iterations)
+}
