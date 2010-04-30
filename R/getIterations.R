1719205c3c18de12c1f99f2ffea3092d4c3328b5
fgochez <fgochez@mango-solutions.com>
2010-04-30 15:32:44 +0000

    issue 2479. (TA121) Adding function header
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@17366 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/getIterations.R
+++ R/getIterations.R
@@ -1,9 +1,22 @@
-# $LastChangedDate: $
-# $LastChangedBy: $
-# $Rev: $
+# $LastChangedDate$
+# $LastChangedBy$
+# $Rev$
 # 
 # Author: fgochez
 ###############################################################################
+
+#' This generic routine extracts the parameter estimate iteration information of a NONMEM problem, 
+#' as produced within the report file or NONMEM 7 iteration file as applicable.  
+#' @param obj Object of class NMRun, NMBasicModel, or NMBasicModelNM7
+#' @param problemNum 
+#' @title Extract parameter estimate iteration information from a given NONMEM problem
+#' @return If an object produced by NONMEM <= 6 was selected, a data.frame of parsed information from the 
+#' report file.  For NONMEM 7, a list generated from the iteration files. The contents of each file are imported
+#' and then aggregated into a single list split by estimation method.
+#' @author fgochez
+#' @keyords utilities
+#' @export
+
 
 getIterations <- function(obj, problemNum = 1)
 {
