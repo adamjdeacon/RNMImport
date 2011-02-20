02fc71dbf0b11c48608085d13d221cc2938c69da
fgochez <fgochez@mango-solutions.com>
2011-02-20 17:58:38 +0000

    issue 2479: adding documentation for importing of NONMEM 7 iterations for importNm (updating function header and auto-generated Rd file), adding documentation for getIterations
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@25204 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/getIterations.R
+++ R/getIterations.R
@@ -1,22 +1,24 @@
-# $LastChangedDate$
-# $LastChangedBy$
-# $Rev$
+# SVN revision: $Rev$
+# Date of last change: $LastChangedDate$
+# Last changed by: $LastChangedBy$
 # 
-# Author: fgochez
+# Original author: fgochez
+# Copyright Mango Solutions, Chippenham, UK
 ###############################################################################
+
 
 #' This generic routine extracts the parameter estimate iteration information of a NONMEM problem, 
 #' as produced within the report file or NONMEM 7 iteration file as applicable.  
-#' @param obj Object of class NMRun, NMBasicModel, or NMBasicModelNM7
-#' @param problemNum 
 #' @title Extract parameter estimate iteration information from a given NONMEM problem
+#' @aliases getIterations,NMRun-method getIterations,NMBasicModel-method getIterations,NMBasicModelNM7-method
+#' @param obj Object of class NMRun, NMBasicModel, or NMBasicModelNM7
+#' @param problemNum Number of the problem of NONMEM object 
 #' @return If an object produced by NONMEM <= 6 was selected, a data.frame of parsed information from the 
 #' report file.  For NONMEM 7, a list generated from the iteration files. The contents of each file are imported
 #' and then aggregated into a single list split by estimation method.
-#' @author fgochez
-#' @keyords utilities
+#' @author Mango Solutions
+#' @keywords utilities
 #' @export
-
 
 getIterations <- function(obj, problemNum = 1)
 {
