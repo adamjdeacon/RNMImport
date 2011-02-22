111328c3f846649b009de4df9463a8740d703d4b
fgochez <fgochez@mango-solutions.com>
2011-02-22 11:58:41 +0000

    refactoring.  Documentation can now be auto-generated from header of getMethodNames, and the routine has been moved to its own file
    git-svn-id: http://svn.mango.local/svn/MangoRepository/RNONMEM2/trunk@25362 4c17f647-02f2-7e4c-97ba-2b8bca437fff

--- R/retrieveMisc.R
+++ R/retrieveMisc.R
@@ -236,45 +236,6 @@ getNmVersion.NMRunProb <- function(obj)
 setMethod("getNmVersion", signature(obj = "NMRun"), getNmVersion.NMRunProb)
 setMethod("getNmVersion", signature(obj = "NMProblem"), getNmVersion.NMRunProb)
 
-
-#' Extracts estimation method names from a run or problem that used NONMEM 7.  These
-#' can be the names used in the control file, or the report file
-#' @param obj NMRun, NMBasicModelNM7, or NMSimModelNM7
-#' @param what [C,1] The string "report" or "control" - selects which file should be used
-#' for the method names
-#' @param problemNum Problem number in run
-#' @title Get method names
-#' @return A character vector of method names used
-#' @author fgochez
-#' @export
-
-getMethodNames <- function(obj, what = c("report", "control" ), problemNum = 1)
-{
-	RNMImportStop("This function is not implemented for objects of this class")
-}
-
-setGeneric("getMethodNames")
-
-getMethodNames.NMRun <- function(obj, what = c("report", "control" ), problemNum = 1)
-{
-	what <- match.arg(what)
-	getMethodNames( getProblem(obj, problemNum = problemNum), what = what )
-}
-
-setMethod("getMethodNames", signature(obj = "NMRun"), getMethodNames.NMRun )
-
-getMethodNames.NMProblemNM7 <- function(obj, what = c("report", "control"), problemNum = 1)
-{
-	what <- match.arg(what)
-	if(what == "report")
-		obj@methodNames
-	else
-		unname(obj@methodInfo[,"method"])
-}
-
-setMethod("getMethodNames", signature(obj = "NMBasicModelNM7"), getMethodNames.NMProblemNM7)
-setMethod("getMethodNames", signature(obj = "NMSimModelNM7"), getMethodNames.NMProblemNM7)
-
 #' Extract information simulation information from a simulation problem
 #' @param obj Object of class NMRun, or NMSim*
 #' @param problemNum Number of problem (applicable only when obj is of class NMRun)
